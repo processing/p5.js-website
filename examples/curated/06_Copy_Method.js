fa6b22305087e384fb4587baa473f756e32cad5b
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-20 23:02:51 -0700

    Code draft

--- examples/curated/06_Copy_Method.js
+++ examples/curated/06_Copy_Method.js
@@ -1,21 +1,33 @@
 /*
  * @name Copy() method
- * @arialabel Parrot rendered in black and white. The user’s cursor is a paint brush and as the user presses and holds on the image, the area becomes colored
  * @frame 600,400
- * @description An example of how to simulate coloring image with the copy() method.
+ * @description An example of how to simulate coloring image with the copy() method. [loads image, uses copy to do something to the images]
  */
-let draft, ready;
+//Define your global variables: bottomImg and topImg.
+let bottomImg, topImg;
+
 function preload() {
-  ready = loadImage('assets/parrot-color.png');
-  draft = loadImage('assets/parrot-bw.png');
+  //Preload the images from the canvas' assets directory.
+  //The bottomImg is the photograph with color,
+  //and the topImg is the black-and-white photograph.
+  bottomImg = loadImage('assets/parrot-color.png');
+  topImg = loadImage('assets/parrot-bw.png');
 }
 function setup() {
   createCanvas(600, 400);
+
+  //Hide the cursor and replace it with a picture of 
+  //a paint brush.
   noCursor();
   cursor('assets/brush.png', 20, -10);
-  image(ready, 0, 0);
-  image(draft, 0, 0);
+
+  //Load the images on top of each other.
+  image(bottomImg, 0, 0);
+  image(topImg, 0, 0);
 }
 function mouseDragged() {
-  copy(ready, mouseX, mouseY, 20, 20, mouseX, mouseY, 20, 20);
+  //Using the copy() function, copy the bottom image
+  //on top of the top image when you drag your cursor
+  //across the canvas.
+  copy(bottomImg, mouseX, mouseY, 20, 20, mouseX, mouseY, 20, 20);
 }
