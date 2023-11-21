c558a7d1e651bcb45ae7ff243c82be3566fb332b
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-20 20:41:58 -0700

    Code revisions

--- examples/curated/15_Input_Button.js
+++ examples/curated/15_Input_Button.js
@@ -1,37 +1,53 @@
 /*
  * @name Input and Button
- * @arialabel “What is your name?” is written in the top left of the window with a text input box and a submit button under. After inputting text and submitting, the text submitted is generated multiple times to cover the background in a random formation in various shades of cyan.
- * @description Input text and click the button to see it affect the the canvas.
+ * @description Input text and click the button to see it affect the the canvas. [shows in-time live updates in the canvas, shows how to tie a button and label to an input]
  */
+
+// Define your global variables: input, button, and greeting.
 let input, button, greeting;
 
 function setup() {
-  // create canvas
   createCanvas(710, 400);
+  background(255);
 
+  //Use the greeting variable to ask for the person's name.
+  greeting = createElement('h2', 'what is your name?');
+  greeting.position(20, 5);
+
+  //Create your input and button in the canvas. (Note to self: add labeling and proper a11y.)
   input = createInput();
   input.position(20, 65);
 
   button = createButton('submit');
   button.position(input.x + input.width, 65);
+
+  //Use the mousePressed() method to call the greet() 
+  //function when the button is pressed.
   button.mousePressed(greet);
-
-  greeting = createElement('h2', 'what is your name?');
-  greeting.position(20, 5);
-
-  textAlign(CENTER);
-  textSize(50);
 }
 
 function greet() {
-  const name = input.value();
-  greeting.html('hello ' + name + '!');
+  //Refresh the canvas background to clear of any
+  //previous inputs.
+  background(255);
+
+  //Connect the name variable to the input's value.
+  let name = input.value();
+
+  //Update the greeting to state the person's name.
+  greeting.html('Hello, ${name}!');
+
+  //Clear the input's value.
   input.value('');
 
+  //Randomly populate the input's value 200 times
+  //within the canvas.
   for (let i = 0; i < 200; i++) {
     push();
     fill(random(255), 255, 255);
     translate(random(width), random(height));
+
+    //no pi
     rotate(random(2 * PI));
     text(name, 0, 0);
     pop();
