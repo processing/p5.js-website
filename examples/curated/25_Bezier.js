19048cd30a7cc923ba5c6c2aaf438caf27acfb59
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-20 22:27:34 -0700

    Code draft

--- examples/curated/25_Bezier.js
+++ examples/curated/25_Bezier.js
@@ -8,22 +8,27 @@
  */
 function setup() {
   createCanvas(720, 400);
+
+  //Set the bezier strokes to white and remove their fill.
   stroke(255);
   noFill();
 }
 
 function draw() {
   background(0);
+
+  //Create 10 bezier lines whose anchor points move 
+  //with your mouse.
   for (let i = 0; i < 200; i += 20) {
     bezier(
-      mouseX - i / 2.0,
+      mouseX - i / 2,
       40 + i,
       410,
       20,
       440,
       300,
-      240 - i / 16.0,
-      300 + i / 8.0
+      240 - i / 16,
+      300 + i / 8
     );
   }
 }
