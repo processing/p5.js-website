4ca0bb789ad03b178bf9e9065553a754e5cf0fbd
Darren Kessner <dkessner@eitm.org>
2023-11-22 09:39:58 -0800

    first draft of example revision

--- examples/curated/01_Continuous_Lines.js
+++ examples/curated/01_Continuous_Lines.js
@@ -1,16 +1,56 @@
 /*
  * @name Continuous Lines
- * @arialabel Thin white line draws on a dark grey background as the user clicks and drags their mouse
  * @description Click and drag the mouse to draw a line.
+ *
+ * This example demonstrates the use of several built-in
+ * variables.  
+ * <a href="https://p5js.org/reference/#/p5/mouseX">mouseX</a>
+ * and
+ * <a href="https://p5js.org/reference/#/p5/mouseY">mouseY</a>
+ * can be used to obtain the current mouse position, while the
+ * previous mouse position is represented by
+ * <a href="https://p5js.org/reference/#/p5/pmouseX">pmouseX</a>
+ * and
+ * <a href="https://p5js.org/reference/#/p5/pmouseY">pmouseY</a>.
+ * 
+ * The 
+ * <a href="https://p5js.org/reference/#/p5/mouseIsPressed">mouseIsPressed</a>.
+ * indicates whether the user is currently pressing the mouse, and
+ * <a href="https://p5js.org/reference/#/p5/frameCount">frameCount</a>
+ * indicates how many frames have been drawn.
+ * 
+ * This example also shows the use of 
+ * <a href="https://p5js.org/reference/#/p5/colorMode">colorMode</a> with HSB
+ * (hue-saturation-brightness), so that the first variable sets the hue.  
+ * The 
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder">remainder</a>
+ * operator is used (frameCount%256) to obtain a number between 0 and 255,
+ * so that the hue changes each frame.
+ *
  */
 function setup() {
+  // Create the canvas
   createCanvas(710, 400);
-  background(102);
+
+  // Set background to black
+  background(0);
+
+  // Set width of the lines
+  strokeWeight(10);
+
+  // Set color mode to hue-saturation-value (HSB)
+  colorMode(HSB);
 }
 
 function draw() {
-  stroke(255);
+  // Change the hue with each frame
+  stroke(frameCount%256, 255, 255);
+
+  // If the mouse button is pressed, draw a line from 
+  // previous mouse position to current mouse position
   if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(pmouseX, pmouseY, mouseX, mouseY);
   }
 }
+
+
