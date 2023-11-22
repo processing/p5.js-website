980cd64642f5434692d85c234e3a3badba5ebcb9
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-21 23:44:56 -0700

    Code and description revisions

--- examples/curated/14_Drop.js
+++ examples/curated/14_Drop.js
@@ -1,38 +1,47 @@
 /*
- * @name Drop
- * @description Using drop, drag an image file onto the canvas to see it displayed. [checks to make sure it's an image file, and offers feedback if its not an img file.]
+ * @name Image Drop
+ * @description <a href="https://p5js.org/reference/#/p5.Element/drop" target="_blank">drop()</a> 
+ * is a p5.js element method that registers a callback every time a file is loaded into the element. 
+ * The uploaded file is created into a <a href="https://p5js.org/reference/#/p5.File" target="_blank">p5.File</a> class.
+ * You can use the <a href="https://p5js.org/reference/#/p5.Element/drop" target="_blank">drop()</a> callback to check 
+ * the file type, then write conditional statements responding to the file type.
  */
-function setup() {
-  //Define your canvas as the image drop area.
-  let dropArea = createCanvas(710, 400);
-  background(100);
+// Define canvasText as a global variable.
+let canvasText = 'Drag an image file onto the canvas.';
 
-  //Add the drop() method to the canvas. Call the gotFile 
-  //function when a file is dropped into the canvas.
+function setup() {  
+  // Assign the dropArea variable to the canvas.
+  let dropArea = createCanvas(710, 400);
+
+  // Add the drop() method to the canvas. Call the gotFile 
+  // function when a file is dropped into the canvas.
   dropArea.drop(gotFile);
 }
 
-function draw() {
-  //Add text in the center of the canvas with drop instructions.
+function draw(){
+  background(100);
+  
+  // Add instructions for dropping an image file in the canvas.
   fill(255);
   noStroke();
   textSize(24);
   textAlign(CENTER);
-  text('Drag an image file onto the canvas.', width / 2, height / 2);
-  noLoop();
+  text(canvasText, width / 2, height / 2);
+  
+  describe(`Grey canvas with the text "${canvasText}" in the center.`);
 }
 
 function gotFile(file) {
-  //If the file dropped into the canvas is an image,
-  //create a variable called img to contain the image.
-  //Remove this image file from the DOM, and only
-  //draw the image within the canvas.
+  // If the file dropped into the canvas is an image,
+  // create a variable called img to contain the image.
+  // Remove this image file from the DOM and only
+  // draw the image within the canvas.
   if (file.type === 'image') {
     let img = createImg(file.data).hide();
     image(img, 0, 0, width, height);
   } else {
-    //If the file dropped into the canvas is not an image,
-    //submit 'Not an image file!' into the browser console.
-    console.log('Not an image file!');
+    // If the file dropped into the canvas is not an image,
+    // change the instructions to 'Not an image file!'
+    canvasText= 'Not an image file!';
   }
 }
