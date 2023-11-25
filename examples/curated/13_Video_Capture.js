e9ee41f3de6ddc60839cd1e145b94d2b05a52462
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-25 12:28:32 -0700

    Code revisions

--- examples/curated/13_Video_Capture.js
+++ examples/curated/13_Video_Capture.js
@@ -1,30 +1,32 @@
 /*
  * @name Video Capture
- * @arialabel Takes feed from the user’s computer camera and displays it in the window
  * @frame 710,240
- * @description Capture video from the webcam and display
- * on the canvas as well with invert filter. Note that by
- * default the capture feed shows up, too. 
+ * @description Using the <a href="https://p5js.org/reference/#/p5/createCapture" target="_blank">createCapture()</a> 
+ * and <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> functions, you can take a device's 
+ * video capture and draw it in the canvas. Since the video capture is passed through the 
+ * <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> constructor, you can add filters to the 
+ * video capture with the <a href="https://p5js.org/reference/#/p5/filter" target="_blank">filter()</a> method.
  */
-//Set capture as a global variable.
+// Set the video capture as a global variable.
 let capture;
 
 function setup() {
+  describe('Video capture from the device webcam.');
   createCanvas(390, 240);
 
-  //Use the createCapture() function to access the device's
-  //camera and start capturing video.
+  // Use the createCapture() function to access the device's
+  // camera and start capturing video.
   capture = createCapture(VIDEO);
 
-  //Make the capture size the size of the canvas.
+  // Make the capture frame the size of the canvas.
   capture.size(320, 240);
 }
 
 function draw() {
   background(255);
 
-  //Draw the resulting video capture on the canvas,
-  //with the invert filter applied.
+  // Draw the resulting video capture on the canvas
+  // with the invert filter applied.
   image(capture, 0, 0, 320, 240);
   filter(INVERT);
 }
