ed603d5c50fe77980c848d75444e94c1263c92e0
Darren Kessner <dkessner@eitm.org>
2023-11-26 09:05:32 -0800

    first draft example revision

--- examples/curated/20_Random.js
+++ examples/curated/20_Random.js
@@ -1,20 +1,52 @@
 /*
  * @name Random
- * @arialabel Various shades of grey bars change patterns randomly every half a second 
- * @description Random numbers create the basis of this image.
- * Each time the program is loaded the result is different.
+ * @description This example demonstrates the use of the
+ * <a href="https://p5js.org/reference/#/p5/random">random()</a>
+ * function.
+ * 
+ * When the user presses the mouse button, the position and color
+ * of the circle change randomly.
  */
+
+
+// Declare variables for the position and color of the circle
+let x;
+let y;
+let c;
+
+
 function setup() {
   createCanvas(710, 400);
-  background(0);
-  strokeWeight(20);
-  frameRate(2);
+
+  // Set the initial position and color of the circle
+  setPositionAndColor();
+
+  describe('A circle with random position and color on the canvas.');
 }
 
-function draw() {
-  for (let i = 0; i < width; i++) {
-    let r = random(255);
-    stroke(r);
-    line(i, 0, i, height);
-  }
+
+function setPositionAndColor(){
+    // Set the position to a random value (within the canvas)
+    x = random(0, width);
+    y = random(0, height);
+
+    // Set R, G, and B to random values in the range [0, 256)
+    c = color(random(256), random(256), random(256));
 }
+
+
+function draw() {
+  background(0);
+
+  // Draw a circle at (x,y) with color c
+  fill(c);
+  circle(x, y, 100);
+}
+
+
+function mousePressed() {
+  // On mouse press (re)set the position and color
+  setPositionAndColor();
+}
+
+
