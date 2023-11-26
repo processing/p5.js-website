cb2e797bc58fd53a51e3953205231c8128759942
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-26 11:22:43 -0700

    Code revisions

--- examples/curated/12_Video_Canvas.js
+++ examples/curated/12_Video_Canvas.js
@@ -1,41 +1,60 @@
 /*
  * @name Video Canvas
- * @description Load a video with multiple formats and draw it to the canvas.
- * To run this example locally, you will need a running 
- * <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.
+ * @description Using the <a href="https://p5js.org/reference/#/p5/createVideo" target="_blank">createVideo()</a> 
+ * and <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> functions, you can upload a video into 
+ * the canvas. Since the video capture is passed through the 
+ * <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> constructor, you can add filters to the 
+ * video capture using the <a href="https://p5js.org/reference/#/p5/filter" target="_blank">filter()</a> method. To run this example locally, 
+ * you will need a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.
  */
-
-//Define video as a global variable.
-let fingers;
+// Define video and playing as global variables.
+// Set playing to false so the videos are paused when loaded into the canvas.
+let video;
+let playing = false;
 
 function setup() {
   createCanvas(710, 400);
 
-  //Upload your video in the canvas' assets directory, and
-  //use the createVideo() function to load in your video.
-  //It's best to upload multiple video formats so your video
-  //is visible within different browsers.
-  fingers = createVideo(['assets/fingers.mov', 'assets/fingers.webm']);
+  // Upload the video in the canvas' assets directory, and
+  // use the createVideo() function to load the video into the code.
+  // It's best to upload multiple video formats so the video
+  // is visible within different browsers.
+  video = createVideo(['assets/fingers.mov', 'assets/fingers.webm']);
   
-  //Use the hide() method to remove the DOM instance of the video.
-  fingers.hide();
+  // By default, the video will render as its own DOM element. 
+  // Use the hide() method to remove the DOM instance of the video.
+  video.hide();
 }
 
 function draw() {
-  background(150);
+  describe('Two videos in the top right and bottom center of the canvas, with the text "Click the canvas to start and pause the video feed." in the top left corner.');
+  background(240);
 
-  //Draw the first instance of the video in the canvas.
-  image(fingers, 10, 10);
+  // Add instructional text to the top right of the canvas.
+  textSize(16);
+  text('Click the canvas to start and pause the video feed.', 345, 50);
 
-  //Draw the second instance of the video, adding a grey
-  //filter to the image.
+  // Draw the first instance of the video in the canvas.
+  image(video, 10, 10);
+
+  // Draw the second instance of the video, adding a grey
+  // filter to the image.
   filter(GRAY);
-  image(fingers, 150, 150);
-
-  fingers.loop();
+  image(video, 150, 150);
 }
 
 function mousePressed() {
-  //Click the canvas to start playing the videos.
-  fingers.loop();
+
+  // When the canvas is clicked, check to see if the videos are
+  // paused or playing. If it is playing, pause the videos.
+  if (playing) {
+    video.pause();
+  } else {
+
+  // If it is paused, play the videos.
+    video.loop();
+  }
+
+  // Change the playing value to the opposite boolean.
+  playing = !playing;
 }
