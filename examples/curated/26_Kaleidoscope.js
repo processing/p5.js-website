69f8dd2fda1c8f7535c82f9c8b2b7903e2069413
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-26 23:59:08 -0700

    Code revisions

--- examples/curated/26_Kaleidoscope.js
+++ examples/curated/26_Kaleidoscope.js
@@ -1,68 +1,52 @@
 /*
  * @name Kaleidoscope
- * @arialabel User draws thick black lines on the grey background and it is mirrored 5 times in a circle like a kaleidoscope
- * @description A kaleidoscope is an optical instrument with two or more reflecting surfaces tilted to each other in an angle. This example tries to replicate the behavior of a kaleidoscope. Set the number of reflections at the symmetry variable and start drawing on the screen. Adjust the brush size with the help of the slider. The clear screen as it says clears the screen. The save button will download a .jpg file of the art that you have created.
+ * @description A kaleidoscope is an optical instrument with two or more reflecting surfaces tilted to each other in an angle. 
+ * Using the <a href="https://p5js.org/reference/#/p5/translate" target="_blank">translate()</a>, 
+ * <a href="https://p5js.org/reference/#/p5/rotate" target="_blank">rotate()</a>, 
+ * and <a href="https://p5js.org/reference/#/p5/scale" target="_blank">scale()</a> functions, you replicate the behavior of a kaleidoscope in a canvas. 
  */
-// Symmetry corresponding to the number of reflections. Change the number for different number of reflections 
+// Define the global variables.
+// The symmetry variable will define how many reflective sections the canvas
+// is split into. 
 let symmetry = 6;   
 
+// The angle button will calculate the angle in which the each section is rotated.
 let angle = 360 / symmetry;
-let saveButton, clearButton, mouseButton, keyboardButton;
-let slider;
 
 function setup() { 
-  createCanvas(710, 710);
+  describe(`Dark grey canvas that reflects the lines drawn within it in ${symmetry} sections.`);
+  createCanvas(710, 400);
   angleMode(DEGREES);
-  background(127);
-
-  // Creating the save button for the file
-  saveButton = createButton('save');
-  saveButton.mousePressed(saveFile);
-
-  // Creating the clear screen button
-  clearButton = createButton('clear');
-  clearButton.mousePressed(clearScreen);
-
-  // Creating the button for Full Screen
-  fullscreenButton = createButton('Full Screen');
-  fullscreenButton.mousePressed(screenFull);
-
-  // Setting up the slider for the thickness of the brush
-  brushSizeSlider = createButton('Brush Size Slider');
-  sizeSlider = createSlider(1, 32, 4, 0.1);
-}
-
-// Save File Function
-function saveFile() {
-  save('design.jpg');
-}
-
-// Clear Screen function
-function clearScreen() {
-  background(127);
-}
-
-// Full Screen Function
-function screenFull() {
-  let fs = fullscreen();
-  fullscreen(!fs);
+  background(50);
 }
 
 function draw() {
+  // Move the 0,0 coordinates of the canvas to the center, instead of in
+  // the top left corner.
   translate(width / 2, height / 2);
 
+  // If the cursor is within the limits of the canvas...
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    
+    // Translate the current position and the previous position of the 
+    // cursor to the new coordinates set with the translate() function above.
     let mx = mouseX - width / 2;
     let my = mouseY - height / 2;
     let pmx = pmouseX - width / 2;
     let pmy = pmouseY - height / 2;
     
+    // And, if the mouse is pressed while in the canvas...
     if (mouseIsPressed) {
+
+      // For every reflective section the canvas is split into, draw the cursor's
+      // coordinates while pressed...
       for (let i = 0; i < symmetry; i++) {
         rotate(angle);
-        let sw = sizeSlider.value();
-        strokeWeight(sw);
+        stroke(255);
+        strokeWeight(2);
         line(mx, my, pmx, pmy);
+
+        // ... and also reflect the line within the symmetry sections as well.
         push();
         scale(1, -1);
         line(mx, my, pmx, pmy);
