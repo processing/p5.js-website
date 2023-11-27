f937c1c293b0f5c7559f48bac21184418d9b9aa2
katlich112358 <kathrynlichlyter@gmail.com>
2023-11-27 00:50:01 -0700

    Code revisions

--- examples/curated/28_Lyric_Poem.js
+++ examples/curated/28_Lyric_Poem.js
@@ -1,45 +1,52 @@
-let lyric =
-  "Are you lonesome tonight? Do you miss me tonight? Are you sorry we drifted apart? Does your memory stray to a bright summer day When I kissed you and called you sweetheart? Do the chairs in your parlor seem empty and bare? Do you gaze at your doorstep and picture me there? Is your heart filled with pain, shall I come back again? Tell me dear, are you lonesome tonight? I wonder if youre lonesome tonight You know someone said that the worlds a stage and each of us must play a part Fate had me playing in love with you as my sweetheart Act one was where we met I loved you at first glance You read your lines so cleverly and never missed a cue Then came act two, you seemed to change, you acted strange And why Ive never known Honey, you lied when you said you loved me And I had no cause to doubt you But Id rather go on hearing your lies Than to go on living without you Now the stage is bare and Im standing there With emptiness all around And if you wont come back to me Then they can bring the curtain down Is your heart filled with pain Shall I come back again? Tell me dear, are you lonesome tonight?";
+/* 
+ * @name Lyric Poem
+ * @description Using the <a href="https://p5js.org/reference/#/p5/floor" target="_blank">floor()</a> and 
+ * <a href="https://p5js.org/reference/#/p5/random" target="_blank">random()</a> functions, you can randomly select a series 
+ * of items from an array and draw them on the canvas.
+*/
+// Define the global variables.
+// Create the lyrics array to store the available lyrics.
+let lyrics = ['Are', 'you', 'lonesome', 'tonight?', 'Do', 'you', 'miss', 'me', 'tonight?', 
+  'Are', 'you', 'sorry', 'we', 'drifted', 'apart?', 'Does', 'your', 'memory', 'stray', 'to', 'a', 
+  'bright', 'summer', 'day', 'When', 'I', 'kissed', 'you', 'and', 'called', 'you', 'sweetheart?', 
+  'Do', 'the', 'chairs', 'in', 'your', 'parlor', 'seem', 'empty', 'and', 'bare?', 'Do', 'you', 'gaze', 
+  'at', 'your', 'doorstep', 'and', 'picture', 'me', 'there?'];
 
-var RSeed = 0;
-var words = 15;
-var Hue = 0;
-var spaceCounter = 1;
+// Set the amount of words to be drawn on the canvas, as 
+// well as the starting hue value. Declare the position variable,
+// which will be used to randomly start the lyrics selection in the array.
+let words = 15;
+let hue;
+let position;
 
 function setup() {
-  createCanvas(500, 500);
+  describe('A random series of words from an array drawn onto the canvas.');
+
+  //Import the selected font style defined the canvas' style.css file.
+  textFont("Space Mono");
+
+  createCanvas(710, 400);
+
+  // Set the text alignment to center and set the color mode to HSB.
   textAlign(CENTER);
-  RSeed = random(0, 9999);
   colorMode(HSB, 255);
 
-  //USING GOOGLE FONTS, IMPORTED IN STYLE.CSS
-  textFont("Space Mono");
-}
+  // Define hue as a random value between 230 and 255.
+  hue = random(230, 255);
 
-function draw() {
-  randomSeed(RSeed);
-  Hue = random(0, 255);
+  // Define the random starting point for selecting the lyrics in the
+  // array.
+  position = floor(random(0, lyrics.length - words));
 
-  /*SPLIT() BREAKS A STRING INTO PIECES USING A 
-  CHARACTER OR STRING AS THE DELIMITER
-  IN THIS CASE THE DELIMITER IS ' ' */
-  let lyrics = split(lyric, " ");
+  background(hue, 255, 30);
+  let line = 1;
 
-  //FINDING A RANDOM WORD OF THE LYRIC TO START WITH
-  pos = floor(random(0, lyrics.length - words));
-
-  background(Hue, 255, 30);
-  var line = 1;
-
-  for (var i = pos; i < pos + words; i++) {
+  // Using the starting position in the array, draw as many lyrics
+  // set with the words variable in the canvas in random positions.
+  for (let i = position; i < position + words; i++) {
     textSize(32);
-    fill(Hue, 200, random(50, 255));
-    text(lyrics[i], random(70, 430), (470 / words) * line);
+    fill(hue, 200, random(150, 255));
+    text(lyrics[i], random(30, 680), (370 / words) * line);
     line++;
   }
-}
-
-function mouseClicked() {
-  RSeed = random(0, 9999);
-  spaceCounter++;
 }
