84168c2eca76052c71c6d305a4f5cb1c224d3bb8
Caleb Foss <calebfoss@gmail.com>
2023-12-06 13:52:06 -0600

    Final draft - add aria-description - add p5 editor file upload info

--- examples/curated/10_Create_Audio.js
+++ examples/curated/10_Create_Audio.js
@@ -20,7 +20,16 @@ function setup() {
   noCanvas();
 
   // Create audio player using path to audio file
-  audioPlayer = createAudio("assets/piano-loop.mp3");
+  // This can also be a URL for a public file
+  // On the p5 Editor, a file may be uploaded to Sketch Files
+  // by clicking the > button on the upper left, followed by the + button
+  audioPlayer = createAudio('assets/piano-loop.mp3');
+
+  // Add description for assistive technologies to explain playback speed
+  audioPlayer.attribute(
+    'aria-description',
+    'The playback speed of this audio player is controlled by the position of the mouse. The further to the right the mouse is, the faster the audio will play.'
+  );
 
   // Display player controls
   audioPlayer.showControls();
