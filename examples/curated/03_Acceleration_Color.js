26bd83c94b41cf43c20c7d34d7c5e3ab333adca4
katlich112358 <kathrynlichlyter@gmail.com>
2023-12-08 09:43:40 -0700

    Final draft

--- examples/curated/03_Acceleration_Color.js
+++ examples/curated/03_Acceleration_Color.js
@@ -1,13 +1,13 @@
-/*
- * @name Acceleration Color
- * @description You can use the <a href="https://p5js.org/reference/#/p5/deviceMoved" target="_blank">deviceMoved()</a> 
- * function to detect when the phone is rotated, and update the background color of the canvas. The background color is mapped to the 
- * <a href="https://p5js.org/reference/#/p5/accelerationX" target="_blank">accelerationX</a>, 
- * <a href="https://p5js.org/reference/#/p5/accelerationY" target="_blank">accelerationY</a>, 
- * and <a href="https://p5js.org/reference/#/p5/accelerationZ" target="_blank">accelerationZ</a> values. 
- * The <a href="https://p5js.org/reference/#/p5/deviceMoved" target="_blank">deviceMoved()</a> function is 
- * available for mobile devices only.
- */
+/**
+ ** @name Acceleration Color
+ ** @description You can use the <a href="https://p5js.org/reference/#/p5/deviceMoved" target="_blank">deviceMoved()</a> 
+ ** function to detect when the phone is rotated, and update the background color of the canvas. The background color is mapped to the 
+ ** <a href="https://p5js.org/reference/#/p5/accelerationX" target="_blank">accelerationX</a>, 
+ ** <a href="https://p5js.org/reference/#/p5/accelerationY" target="_blank">accelerationY</a>, 
+ ** and <a href="https://p5js.org/reference/#/p5/accelerationZ" target="_blank">accelerationZ</a> values. 
+ ** The <a href="https://p5js.org/reference/#/p5/deviceMoved" target="_blank">deviceMoved()</a> function is 
+ ** available for mobile devices only.
+ **/
 // Establish the global variables: redValue, greenValue, and blueValue.
 let redValue, greenValue, blueValue;
 
@@ -19,7 +19,7 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
 
   // Set the redValue, greenValue, and blueValue variables to 
-  //random hue values.
+  // random numbers.
   redValue = random(50, 255);
   greenValue = random(0, 200);
   blueValue = random(50, 255);
@@ -28,6 +28,15 @@ function setup() {
 function draw() {
   // Set the canvas background to the hue values.
   background(redValue, greenValue, blueValue);
+
+  // Make a label in the center of the canvas that the acceleration
+  // values, as well as the current RGB values of the screen.
+  fill(0);
+  stroke(255);
+  strokeWeight(1);
+  textSize(15);
+  text(`Acceleration values: X (${int(accelerationX)}), Y (${int(accelerationY)}), Z (${int(accelerationZ)}).`, 25, 25, displayWidth/2);
+  text(`Canvas color: R (${int(redValue)}), G (${int(greenValue)}), B (${int(blueValue)}).`, 25, 50, displayWidth/2);
 }
 
 function deviceMoved() {
