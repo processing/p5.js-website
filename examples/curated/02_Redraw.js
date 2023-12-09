373b94f3a94f7b66ba53159ff3d0f644642879a5
Darren Kessner <Darren.Kessner@marlborough.org>
2023-12-09 09:32:32 -0500

    added requested changes

--- examples/curated/02_Redraw.js
+++ examples/curated/02_Redraw.js
@@ -4,10 +4,12 @@
  * <a href="https://p5js.org/reference/#/p5/loop">loop()</a>
  * and
  * <a href="https://p5js.org/reference/#/p5/noLoop">noLoop()</a>
- * to control the animation loop.
+ * to pause and resume the animation.
  *
  * Clicking the mouse toggles the animation loop.  If the animation
  * loop is stopped, the user can press any key to advance one frame.
+ * Note: the user must click to set the focus on the canvas for
+ * key presses to be registered.
  * 
  * Advancing a single frame is accomplished by calling the
  * <a href="https://p5js.org/reference/#/p5/redraw">redraw()</a>
@@ -15,7 +17,7 @@
  *
  */
 
-// Position of the circle
+// Declare variable for the horizontal position of the circle
 let x = 25;
 
 function setup() {
@@ -27,36 +29,39 @@ function setup() {
 
   // Set the text size
   textSize(20);
+
+  // No animation to start
+  noLoop();
 }
+
 
 function draw() {
   // Clear the background
   background(0);
 
   // Draw a circle, with hue determined by frameCount
-  fill(frameCount%256, 255, 255);
+  fill(x/3, 90, 90);
   circle(x, height/2, 50);
 
-  // Advance the position
+  // Increase the x variable by 5
   x += 5;
 
   // Reset the circle position after it moves off the right side
-  if (x > width+25)
+  if (x > width+25) {
     x = -25;
+  }
 
-  // Show the value of isLooping()
-  text("isLooping(): " + isLooping(), 25, 25);
-
-  describe('circle at x position ' + x + ' moving to the right');
+  describe('circle moving to the right');
 }
 
 
 function mousePressed() {
   // Start/stop the animation loop
-  if (isLooping()) 
+  if (isLooping()) {
     noLoop();
-  else 
+  } else {
     loop();
+  }
 }
 
 
