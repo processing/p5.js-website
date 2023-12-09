dd9ecd81d8ecc6bbcd44e12403b224b05e348ddc
Darren Kessner <Darren.Kessner@marlborough.org>
2023-12-09 15:53:18 -0500

    added requested changes

--- examples/curated/40_Soft_Body.js
+++ examples/curated/40_Soft_Body.js
@@ -1,19 +1,23 @@
-/*
+/**
  * @name Soft Body
- * @description Softbody dynamics simulation using curveVertex() and 
- * curveTightness().
+ * @description Physics simulation of a soft body experiencing
+ * acceleration toward the mouse position.  The shape is created
+ * with curves using 
+ * <a href="https://p5js.org/reference/#/p5/curveVertex">curveVertex()</a>
+ * and
+ * <a href="https://p5js.org/reference/#/p5/curveTightness">curveTightness()</a>.
  */
 
 
+// Declare variables for the physics calculations
 let centerX = 0.0, centerY = 0.0;
-
 let radius = 45, rotAngle = -90;
 let accelX = 0.0, accelY = 0.0;
 let deltaX = 0.0, deltaY = 0.0;
 let springing = 0.0009, damping = 0.98;
 
+// Declare variables for specifying vertex locations
 let nodes = 5;
-
 let nodeStartX = [];
 let nodeStartY = [];
 let nodeX = [];
@@ -21,20 +25,21 @@ let nodeY = [];
 let angle = [];
 let frequency = [];
 
+// Declare the variable for the curve tightness
 let organicConstant = 1.0;
 
 function setup() {
   createCanvas(710, 400);
 
   // Start in the center of the canvas
-  centerX = width/2;
-  centerY = height/2;
+  centerX = width / 2;
+  centerY = height / 2;
 
   // Initialize arrays to 0
   for (let i = 0; i < nodes; i++){
     nodeStartX[i] = 0;
     nodeStartY[i] = 0;
-    nodeY[i] = 0;
+    nodeX[i] = 0;
     nodeY[i] = 0;
     angle[i] = 0;
   }
@@ -45,12 +50,12 @@ function setup() {
   }
 
   noStroke();
+  angleMode(DEGREES);
 }
 
 function draw() {
   // Use alpha blending for fade effect
-  fill(0, 50);
-  rect(0, 0, width, height);
+  background(0, 50);
 
   // Draw and move the shape
   drawShape();
@@ -60,15 +65,16 @@ function draw() {
 function drawShape() {
   // Calculate node starting locations
   for (let i = 0; i < nodes; i++){
-    nodeStartX[i] = centerX + cos(radians(rotAngle)) * radius;
-    nodeStartY[i] = centerY + sin(radians(rotAngle)) * radius;
+    nodeStartX[i] = centerX + cos(rotAngle) * radius;
+    nodeStartY[i] = centerY + sin(rotAngle) * radius;
     rotAngle += 360.0 / nodes;
   }
 
   // Draw the polygon
 
   curveTightness(organicConstant);
-  fill(lerpColor(color('red'), color('yellow'), 1-organicConstant));
+  let shapeColor = lerpColor(color('red'), color('yellow'), organicConstant);
+  fill(shapeColor);
 
   beginShape();
   for (let i = 0; i < nodes; i++){
@@ -96,13 +102,14 @@ function moveShape() {
   accelX *= damping;
   accelY *= damping;
 
-  // Change curve tightness
+  // Change curve tightness based on the overall acceleration;
+  // use abs() to avoid dependence on direction of acceleration
   organicConstant = 1 - ((abs(accelX) + abs(accelY)) * 0.1);
 
   // Move nodes
   for (let i = 0; i < nodes; i++){
-    nodeX[i] = nodeStartX[i] + sin(radians(angle[i])) * (accelX * 2);
-    nodeY[i] = nodeStartY[i] + sin(radians(angle[i])) * (accelY * 2);
+    nodeX[i] = nodeStartX[i] + sin(angle[i]) * (accelX * 2);
+    nodeY[i] = nodeStartY[i] + sin(angle[i]) * (accelY * 2);
     angle[i] += frequency[i];
   }
 }
