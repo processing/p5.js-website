1ad369941ab2fe797ddf29bbe0a212bf39509b2f
Caleb Foss <calebfoss@gmail.com>
2023-12-14 12:43:07 -0600

    Color, code style, and refactoring: - Switch from default CSS colors to colors to less severe tones - Use camel case for variable names - Add spaces between operators - Unabbreviate variable names with more descriptive names - Use pow() function - Use setup instead of draw with noLoop for simpler structure - Use += 1 instead of ++

--- examples/curated/49_Mandelbrot.js
+++ examples/curated/49_Mandelbrot.js
@@ -1,20 +1,14 @@
 /**
  * @name The Mandelbrot Set
  * @description Colorful rendering of the Mandelbrot set.
- * Based on Daniel Shiffman's 
+ * Based on Daniel Shiffman's
  * <a href="https://processing.org/examples/mandelbrot.html">Mandelbrot Example</a> for Processing.
  */
-
 
 function setup() {
   createCanvas(710, 400);
   pixelDensity(1);
-  noLoop();
   describe('Colorful rendering of the Mandelbrot set.');
-}
-
-
-function draw() {
   background(0);
 
   // Establish a range of values on the complex plane
@@ -23,60 +17,77 @@ function draw() {
   let h = (w * height) / width;
 
   // Start at negative half the width and height
-  let xmin = -w/2;
-  let ymin = -h/2;
+  let xMin = -w / 2;
+  let yMin = -h / 2;
 
   // Access the pixels[] array
   loadPixels();
 
-  // Set the maxium number of iterations for each point on the complex plane
-  let maxiterations = 100;
+  // Set the maximum number of iterations for each point on the complex plane
+  let maxIterations = 100;
 
-  // x goes from xmin to xmax
-  let xmax = xmin + w;
-  // y goes from ymin to ymax
-  let ymax = ymin + h;
+  // x goes from xMin to xMax
+  let xMax = xMin + w;
+
+  // y goes from yMin to yMax
+  let yMax = yMin + h;
 
   // Calculate amount we increment x,y for each pixel
-  let dx = (xmax - xmin) / (width);
-  let dy = (ymax - ymin) / (height);
+  let dx = (xMax - xMin) / width;
+  let dy = (yMax - yMin) / height;
 
   // Start y
-  let y = ymin;
-  for (let j = 0; j < height; j++) {
+  let y = yMin;
+  for (let j = 0; j < height; j += 1) {
     // Start x
-    let x = xmin;
-    for (let i = 0; i < width; i++) {
-
+    let x = xMin;
+    for (let i = 0; i < width; i += 1) {
       // Test whether iteration of z = z^2 + cm diverges
       let a = x;
       let b = y;
-      let n = 0;
-      while (n < maxiterations) {
-        let aa = a * a;
-        let bb = b * b;
-        let twoab = 2.0 * a * b;
-        a = aa - bb + x;
-        b = twoab + y;
+      let iterations = 0;
+      while (iterations < maxIterations) {
+        let aSquared = pow(a, 2);
+        let bSquared = pow(b, 2);
+        let twoAB = 2.0 * a * b;
+        a = aSquared - bSquared + x;
+        b = twoAB + y;
+
         // If the values are too big, stop iteration
-        if (dist(aa, bb, 0, 0) > 16) {
+        if (dist(aSquared, bSquared, 0, 0) > 16) {
           break;
         }
-        n++;
+        iterations += 1;
       }
 
       // Color each pixel based on how long it takes to get to infinity
 
-      let index = (i+j*width)*4;
-      let t = map(n, 0, maxiterations, 0, 1);
+      let index = (i + j * width) * 4;
 
-      let c = color(0);
-      if (n < maxiterations)
-          c = lerpColor(color('blue'), color('yellow'), t);
+      // Convert number of iterations to range of 0-1
+      let normalized = map(iterations, 0, maxIterations, 0, 1);
+
+      // Use square root of normalized value for color interpolation
+      let lerpAmount = sqrt(normalized);
+
+      // Set default color to black
+      let pixelColor = color(0);
+
+      // Blue
+      let startColor = color(47, 68, 159);
+
+      // Light yellow
+      let endColor = color(255, 255, 128);
+
+      // If iteration is under the maximum, interpolate a color
+      if (iterations < maxIterations) {
+        pixelColor = lerpColor(startColor, endColor, lerpAmount);
+      }
 
       // Copy the RGBA values from the color to the pixel
-      for (let i=0; i<4; i++)
-        pixels[index+i] = c.levels[i];
+      for (let i = 0; i < 4; i += 1) {
+        pixels[index + i] = pixelColor.levels[i];
+      }
 
       x += dx;
     }
@@ -84,5 +95,3 @@ function draw() {
   }
   updatePixels();
 }
-
-
