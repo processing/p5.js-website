b585054163ebb4998052a19bb4204f4d008e19a2
Caleb Foss <calebfoss@gmail.com>
2023-12-14 13:50:39 -0600

    Code style adjustments: - spaces between operators - new line before comments when not first in block

--- examples/curated/46_Forces.js
+++ examples/curated/46_Forces.js
@@ -1,13 +1,13 @@
 /**
  * @name Forces
  * @description Demonstration of multiple force acting on bodies.
- * Bodies experience gravity continuously.Bodies experience fluid 
+ * Bodies experience gravity continuously.Bodies experience fluid
  * resistance when in "water".
  * (<a href="http://natureofcode.com">natureofcode.com</a>)
  *
- * The force calculations are performed using the 
+ * The force calculations are performed using the
  * <a href="https://p5js.org/reference/#/p5.Vector">p5.Vector</a>
- * class, including the 
+ * class, including the
  * <a href="https://p5js.org/reference/#/p5/createVector">createVector()</a>
  * function to create vectors.
  *
@@ -25,9 +25,11 @@ function setup() {
   initializeMovers();
 
   // Create Liquid object
-  liquid = new Liquid(0, height/2, width, height/2, 0.1);
+  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
 
-  describe('Nine grey balls drop from the top of the canvas and slow down as they reach the bottom half of the canvas.');
+  describe(
+    'Nine grey balls drop from the top of the canvas and slow down as they reach the bottom half of the canvas.'
+  );
 }
 
 function draw() {
@@ -41,12 +43,14 @@ function draw() {
     if (liquid.contains(mover)) {
       // Calculate drag force
       let dragForce = liquid.calculateDrag(mover);
+
       // Apply drag force to Mover
       mover.applyForce(dragForce);
     }
 
     // Gravitational force is proportional to the mass
     let gravity = createVector(0, 0.1 * mover.mass);
+
     // Apply gravitional force
     mover.applyForce(gravity);
 
@@ -57,114 +61,105 @@ function draw() {
   }
 }
 
-
 function mousePressed() {
   initializeMovers();
 }
 
-
-
 function initializeMovers() {
   // Calculate the spacing based on the width of the canvas
-  let xSpacing = width / 9;  
+  let xSpacing = width / 9;
 
   // Fill the movers array with 9 Mover objects with random masses
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < 9; i += 1) {
     let mass = random(0.5, 3);
     let xPosition = xSpacing * i + xSpacing / 2;
-    movers[i] = new Mover(mass, 
-        xPosition, 0, 
-        color(i, 100, 100));
+    movers[i] = new Mover(mass, xPosition, 0, color(i, 100, 100));
   }
 }
 
-
 class Liquid {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+  }
 
-    constructor(x, y, w, h, c) {
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.c = c;
-    };
+  // Check whether the Mover in the Liquid
+  contains(m) {
+    let l = m.position;
+    return (
+      l.x > this.x &&
+      l.x < this.x + this.w &&
+      l.y > this.y &&
+      l.y < this.y + this.h
+    );
+  }
 
-    // Check whether the Mover in the Liquid
-    contains(m) {
-      let l = m.position;
-      return (
-        l.x > this.x &&
-        l.x < this.x + this.w &&
-        l.y > this.y &&
-        l.y < this.y + this.h
-      );
-    }
+  // Calculate drag force
+  calculateDrag(m) {
+    // The drag force magnitude is coefficient * speed squared
+    let speed = m.velocity.mag();
+    let dragMagnitude = this.c * speed * speed;
 
-    // Calculate drag force
-    calculateDrag(m) {
-      // The drag force magnitude is coefficient * speed squared
-      let speed = m.velocity.mag();
-      let dragMagnitude = this.c * speed * speed;
+    // Create the drag force vector (opposite direction of velocity)
+    let dragForce = m.velocity.copy();
+    dragForce.mult(-1);
 
-      // Create the drag force vector (opposite direction of velocity)
-      let dragForce = m.velocity.copy();
-      dragForce.mult(-1);
+    // Scale the drag force vector to the magnitude calculated above
+    dragForce.setMag(dragMagnitude);
 
-      // Scale the drag force vector to the magnitude calculated above
-      dragForce.setMag(dragMagnitude);
+    return dragForce;
+  }
 
-      return dragForce;
-    }
-
-    display() {
-      noStroke();
-      fill(50);
-      rect(this.x, this.y, this.w, this.h);
-    }
+  display() {
+    noStroke();
+    fill(50);
+    rect(this.x, this.y, this.w, this.h);
+  }
 } // class Liquid
 
-
 class Mover {
+  constructor(m, x, y, c) {
+    this.mass = m;
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+    this.color = c;
+  }
 
-    constructor(m, x, y, c) {
-      this.mass = m;
-      this.position = createVector(x, y);
-      this.velocity = createVector(0, 0);
-      this.acceleration = createVector(0, 0);
-      this.color = c;
-    }
+  // Apply force according to Newton's 2nd law: F = M * A
+  // or A = F / M
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
 
-    // Apply force according to Newton's 2nd law: F = M * A
-    // or A = F / M
-    applyForce(force) {
-      let f = p5.Vector.div(force, this.mass);
-      this.acceleration.add(f);
-    }
+  update() {
+    // Change the velocity by the acceleration
+    this.velocity.add(this.acceleration);
 
-    update() {
-      // Change the velocity by the acceleration
-      this.velocity.add(this.acceleration);
-      // Change the position by the velocity
-      this.position.add(this.velocity);
-      // Clear the acceleration each frame
-      this.acceleration.mult(0);
-    }
+    // Change the position by the velocity
+    this.position.add(this.velocity);
 
-    display() {
-      stroke(0);
-      strokeWeight(2);
-      fill(this.color);
-      ellipse(this.position.x, this.position.y, this.mass * 16, this.mass * 16);
-    }
+    // Clear the acceleration each frame
+    this.acceleration.mult(0);
+  }
 
-    // Make the balls bounce at the bottom
-    checkEdges() {
-      if (this.position.y > height - this.mass * 8) {
-        // A little dampening when hitting the bottom
-        this.velocity.y *= -0.9;
-        this.position.y = height - this.mass * 8;
-      }
+  display() {
+    stroke(0);
+    strokeWeight(2);
+    fill(this.color);
+    ellipse(this.position.x, this.position.y, this.mass * 16, this.mass * 16);
+  }
+
+  // Make the balls bounce at the bottom
+  checkEdges() {
+    if (this.position.y > height - this.mass * 8) {
+      // A little dampening when hitting the bottom
+      this.velocity.y *= -0.9;
+      this.position.y = height - this.mass * 8;
     }
+  }
 } // class Mover
-
-
