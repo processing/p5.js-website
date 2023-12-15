a66c2d05528f8103883548c1a1fdf1b3b5afb8c5
Caleb Foss <calebfoss@gmail.com>
2023-12-15 14:17:32 -0600

    Remove extra * in JSDoc, use preload

--- examples/curated/08_Transparency.js
+++ examples/curated/08_Transparency.js
@@ -1,25 +1,31 @@
 /**
- ** @name Image Transparency
- ** @description This program overlays one image over another by modifying the
- ** alpha value of the image with the <a href="https://p5js.org/reference/#/p5/tint" target="_blank">tint()</a> 
- ** function. Move the cursor left and right across the canvas to change the image's position. To run this example 
- ** locally, you will need an image file, and a running 
- ** <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.
- **/
+ * @name Image Transparency
+ * @description This program overlays one image over another by modifying the
+ * alpha value of the image with the
+ * <a href="https://p5js.org/reference/#/p5/tint" target="_blank">tint()</a>
+ * function. Move the cursor left and right across the canvas to change the
+ * image's position. To run this example
+ * locally, you will need an image file, and a running
+ * <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.
+ */
 // Define the global variables: img, offset, and easing.
-// Set offset to 0 and easing to 0.05 for moving the 
+// Set offset to 0 and easing to 0.05 for moving the
 // transparent image with the cursor position.
 let img;
 let offset = 0;
 let easing = 0.05;
 
-function setup() {
-  describe('An astronaut on planet as the background with a slightly transparent version of this image on top that moves with the horizontal direction of the user’s mouse.');
-  
-  createCanvas(720, 400);
-
+function preload() {
   // Load the bottom image from the canvas' assets directory.
   img = loadImage('assets/moonwalk.jpg');
+}
+
+function setup() {
+  describe(
+    "An astronaut on planet as the background with a slightly transparent version of this image on top that moves with the horizontal direction of the user's mouse."
+  );
+
+  createCanvas(720, 400);
 }
 
 function draw() {
