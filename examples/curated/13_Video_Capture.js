d75e7e2efd76a3d7be724c6053c8f4237e2a6a2c
Caleb Foss <calebfoss@gmail.com>
2023-12-15 12:04:04 -0600

    Remove extra * in JSDoc

--- examples/curated/13_Video_Capture.js
+++ examples/curated/13_Video_Capture.js
@@ -1,15 +1,15 @@
 /**
- ** @name Video Capture
- ** @frame 710,240
- ** @description Using the <a href="https://p5js.org/reference/#/p5/createCapture" target="_blank">createCapture()</a> 
- ** and <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> functions, you can take a device's 
- ** video capture and draw it in the canvas. Since the video capture is passed through the 
- ** <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> constructor, you can add filters to the 
- ** video capture with the <a href="https://p5js.org/reference/#/p5/filter" target="_blank">filter()</a> method. 
- ** For different strategies for uploading, presenting, or styling videos, visit the 
- ** <a href="https://p5js.org/examples/dom-video.html" target="_blank">Video</a> and 
- ** <a href="https://p5js.org/examples/dom-video-canvas.html" target="_blank">Video Canvas</a> examples.
- **/
+ * @name Video Capture
+ * @frame 710,240
+ * @description Using the <a href="https://p5js.org/reference/#/p5/createCapture" target="_blank">createCapture()</a>
+ * and <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> functions, you can take a device's
+ * video capture and draw it in the canvas. Since the video capture is passed through the
+ * <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> constructor, you can add filters to the
+ * video capture with the <a href="https://p5js.org/reference/#/p5/filter" target="_blank">filter()</a> method.
+ * For different strategies for uploading, presenting, or styling videos, visit the
+ * <a href="https://p5js.org/examples/dom-video.html" target="_blank">Video</a> and
+ * <a href="https://p5js.org/examples/dom-video-canvas.html" target="_blank">Video Canvas</a> examples.
+ */
 // Set the video capture as a global variable.
 let capture;
 
@@ -25,7 +25,7 @@ function setup() {
   capture.size(360, 200);
 
   // Use capture.hide() to remove the p5.Element object made
-  // using createCapture(). The video will instead be rendered as 
+  // using createCapture(). The video will instead be rendered as
   // an image in draw().
   capture.hide();
 }
