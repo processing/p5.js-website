b00034b32891fb5fb130a5ee151b192a6bb57edc
Caleb Foss <calebfoss@gmail.com>
2023-12-15 11:22:26 -0600

    Adjust spacing before comments to match code style guide

--- examples/curated/27_Recursive_Tree.js
+++ examples/curated/27_Recursive_Tree.js
@@ -19,25 +19,31 @@ function draw() {
   // Calculate the angle based on the mouse position, maximum 90 degrees
   let angle = (mouseX / width) * 90;
   angle = min(angle, 90);
+
   // Convert the angle to radians
   theta = radians(angle);
+
   // Start the tree from the bottom of the screen
-  translate(width/2, height);
+  translate(width / 2, height);
+
   // Draw a line 120 pixels
   stroke(0, 255, 255);
   line(0, 0, 0, -120);
+
   // Move to the end of that line
   translate(0, -120);
+
   // Start the recursive branching
   branch(120, 0);
 
-  describe('A tree drawn by recursively drawing branches, with angle determined by the user mouse position.');
+  describe(
+    'A tree drawn by recursively drawing branches, with angle determined by the user mouse position.'
+  );
 }
 
 function branch(h, level) {
-
   // Set the hue based on the recursion level
-  stroke(level*25, 255, 255);
+  stroke(level * 25, 255, 255);
 
   // Each branch will be 2/3rds the size of the previous one
   h *= 0.66;
@@ -46,15 +52,20 @@ function branch(h, level) {
   if (h > 2) {
     // Draw the right branch
     // Save the current coordinate system
-    push();    
+    push();
+
     // Rotate by theta
     rotate(theta);
+
     // Draw the branch
-    line(0, 0, 0, -h);  
+    line(0, 0, 0, -h);
+
     // Move to the end of the branch
-    translate(0, -h); 
+    translate(0, -h);
+
     // Call branch() recursively
-    branch(h, level+1);       
+    branch(h, level + 1);
+
     // Restore the saved coordinate system
     pop();
 
@@ -63,8 +74,7 @@ function branch(h, level) {
     rotate(-theta);
     line(0, 0, 0, -h);
     translate(0, -h);
-    branch(h, level+1);
+    branch(h, level + 1);
     pop();
   }
 }
-
