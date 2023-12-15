ee250b9e81de8cb2846dde8144c6019c251418a6
Caleb Foss <calebfoss@gmail.com>
2023-12-15 14:38:41 -0600

    Remove for loop and transformations, split variable declarations

--- examples/curated/15_Input_Button.js
+++ examples/curated/15_Input_Button.js
@@ -1,12 +1,14 @@
 /**
  ** @name Input and Button
- ** @description Using the <a href="https://p5js.org/reference/#/p5.Element/createElement" target="_blank">createElement()</a>, 
- ** <a href="https://p5js.org/reference/#/p5/createInput" target="_blank">createInput()</a>, 
+ ** @description Using the <a href="https://p5js.org/reference/#/p5.Element/createElement" target="_blank">createElement()</a>,
+ ** <a href="https://p5js.org/reference/#/p5/createInput" target="_blank">createInput()</a>,
  ** and <a href="https://p5js.org/reference/#/p5.Element/createButton" target="_blank">createButton()</a> functions,
  ** you can take a string of text submitted via text input and transpose it into multiple elements within your canvas.
  **/
 // Define the global variables: input, button, and greeting.
-let nameInput, button, greeting;
+let nameInput;
+let button;
+let greeting;
 
 function setup() {
   createCanvas(710, 400);
@@ -23,9 +25,11 @@ function setup() {
   button = createButton('submit');
   button.position(nameInput.x + nameInput.width, 65);
 
-  // Use the mousePressed() method to call the greet() 
+  // Use the mousePressed() method to call the greet()
   // function when the button is pressed.
   button.mousePressed(greet);
+
+  // Also call greet when input is changed and enter/return button is pressed
   nameInput.changed(greet);
 }
 
@@ -43,15 +47,10 @@ function greet() {
   // Clear the input's value.
   nameInput.value('');
 
-  // Randomly populate the input's value 200 times
-  // within the canvas.
-  for (let i = 0; i < 200; i++) {
-    push();
-    fill(random(0, 180));
-    translate(random(width), random(height));
-    text(name, 0, 0);
-    pop();
-  }
-  
-  describe(`${name} repeated 200 times across a white background.`);
+  // Draw name on the canvas
+  textSize(100);
+  textAlign(CENTER, CENTER);
+  text(name, width / 2, height / 2);
+
+  describe(`The name ${name} in large black text on a white background.`);
 }
