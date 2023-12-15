58d9d64c9835a601540c16ad6ba04e0d4b4e1251
Caleb Foss <calebfoss@gmail.com>
2023-12-15 11:20:22 -0600

    Remove extra * for JSDoc

--- examples/curated/12_Video_Canvas.js
+++ examples/curated/12_Video_Canvas.js
@@ -1,12 +1,12 @@
 /**
- ** @name Video Canvas
- ** @description Using the <a href="https://p5js.org/reference/#/p5/createVideo" target="_blank">createVideo()</a> 
- ** and <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> functions, you can upload a video into 
- ** the canvas. Since the video capture is passed through the 
- ** <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> constructor, you can add filters to the 
- ** video capture using the <a href="https://p5js.org/reference/#/p5/filter" target="_blank">filter()</a> method. To run this example locally, 
- ** you will need a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>. 
- ** To build a video without embedding it within the canvas, visit the <a href="https://p5js.org/examples/dom-video.html">Video</a> example.
+ * @name Video Canvas
+ * @description Using the <a href="https://p5js.org/reference/#/p5/createVideo" target="_blank">createVideo()</a>
+ * and <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> functions, you can upload a video into
+ * the canvas. Since the video capture is passed through the
+ * <a href="https://p5js.org/reference/#/p5/image" target="_blank">image()</a> constructor, you can add filters to the
+ * video capture using the <a href="https://p5js.org/reference/#/p5/filter" target="_blank">filter()</a> method. To run this example locally,
+ * you will need a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.
+ * To build a video without embedding it within the canvas, visit the <a href="https://p5js.org/examples/dom-video.html">Video</a> example.
  **/
 // Define video and playing as global variables.
 // Set playing to false so the videos are paused when loaded into the canvas.
@@ -21,14 +21,16 @@ function setup() {
   // It's best to upload multiple video formats so the video
   // is visible within different browsers.
   video = createVideo(['assets/fingers.mov', 'assets/fingers.webm']);
-  
-  // By default, the video will render as its own DOM element. 
+
+  // By default, the video will render as its own DOM element.
   // Use the hide() method to remove the DOM instance of the video.
   video.hide();
 }
 
 function draw() {
-  describe('Two videos in the top right and bottom center of the canvas, with the text "Click the canvas to start and pause the video feed." in the top left corner.');
+  describe(
+    'Two videos in the top right and bottom center of the canvas, with the text "Click the canvas to start and pause the video feed." in the top left corner.'
+  );
   background(240);
 
   // Add instructional text to the top right of the canvas.
@@ -51,8 +53,7 @@ function mousePressed() {
   if (playing) {
     video.pause();
   } else {
-
-  // If it is paused, play the videos.
+    // If it is paused, play the videos.
     video.loop();
   }
 
