cd09260deff3b2ed33eb15ec6b837cb01c65bcb6
Caleb Foss <calebfoss@gmail.com>
2023-12-15 13:30:34 -0600

    Adjust spacing, rename to Animation with Events

--- examples/curated/02_Redraw.js
+++ examples/curated/02_Redraw.js
@@ -1,6 +1,6 @@
 /**
- * @name Redraw
- * @description This example demonstrates the use of 
+ * @name Animation with Events
+ * @description This example demonstrates the use of
  * <a href="https://p5js.org/reference/#/p5/loop">loop()</a>
  * and
  * <a href="https://p5js.org/reference/#/p5/noLoop">noLoop()</a>
@@ -10,7 +10,7 @@
  * loop is stopped, the user can press any key to advance one frame.
  * Note: the user must click to set the focus on the canvas for
  * key presses to be registered.
- * 
+ *
  * Advancing a single frame is accomplished by calling the
  * <a href="https://p5js.org/reference/#/p5/redraw">redraw()</a>
  * function, which results in a single call to the draw() function.
@@ -23,7 +23,7 @@ let x = 25;
 function setup() {
   // Create the canvas
   createCanvas(720, 400);
- 
+
   // Set the color mode to hue-saturation-brightness (HSB)
   colorMode(HSB);
 
@@ -34,26 +34,24 @@ function setup() {
   noLoop();
 }
 
-
 function draw() {
   // Clear the background
   background(0);
 
   // Draw a circle, with hue determined by frameCount
-  fill(x/3, 90, 90);
-  circle(x, height/2, 50);
+  fill(x / 3, 90, 90);
+  circle(x, height / 2, 50);
 
   // Increase the x variable by 5
   x += 5;
 
   // Reset the circle position after it moves off the right side
-  if (x > width+25) {
+  if (x > width + 25) {
     x = -25;
   }
 
   describe('circle moving to the right');
 }
-
 
 function mousePressed() {
   // Start/stop the animation loop
@@ -64,10 +62,7 @@ function mousePressed() {
   }
 }
 
-
 function keyPressed() {
   // Draw one frame
   redraw();
 }
-
-
