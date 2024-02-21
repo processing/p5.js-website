3a025f15c355e99d0f512a5c861739a55852eceb
evelyn masso <coding@outofambit.com>
2024-02-21 09:50:00 -0800

    add css example

---
+++ src/components/HeaderBar/index.jsx
@@ -0,0 +1,9 @@
+import Styles from "./styles.module.css";
+
+export const HeaderBar = () => {
+  return (
+    <div className={Styles.header}>
+      <h1>p5.js</h1>
+    </div>
+  );
+};
--- src/components/header.jsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <div className="header">
-      <h1>p5.js</h1>
-    </div>
-  );
-};
