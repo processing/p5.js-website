c1682cd558d4aeda9df25948fc382933734aabdd
evelyn masso <coding@outofambit.com>
2024-02-21 11:27:34 -0800

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
       compat: true,
     }),
     mdx(),
+    compress(),
   ],
   trailingSlash: "always",
   build: {
