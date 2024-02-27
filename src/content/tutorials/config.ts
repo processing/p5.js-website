31d269b9a964c461301b68c58df4fde03d4453cb
evelyn masso <coding@outofambit.com>
2024-02-27 15:33:33 -0800

    use alt text for featured images on tutorials

--- src/content/tutorials/config.ts
+++ src/content/tutorials/config.ts
@@ -5,7 +5,12 @@ export const tutorialCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
-    featuredImage: z.string().optional(),
+    featuredImage: z
+      .object({
+        url: z.string().url(),
+        altText: z.string(),
+      })
+      .optional(),
     relatedExamples: z.array(z.string()).optional(),
     // related_examples: z.array(reference("examples")).optional(),
     relatedReferences: z.array(z.string()).optional(),
