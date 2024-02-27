089ff5f5d6f18d85bd2f019dcc791cef0b7fc4ec
evelyn masso <coding@outofambit.com>
2024-02-27 15:29:32 -0800

    add past events example, schema, and routes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,7 @@
 import { tutorialCollection } from "./tutorials/config";
+import { pastEventsCollection } from "./past-events/config";
 
 export const collections = {
   tutorials: tutorialCollection,
+  "past-events": pastEventsCollection,
 };
