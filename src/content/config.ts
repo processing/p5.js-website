2a78f544683388894eaf9b8f26a4aab83c2dbf21
evelyn masso <coding@outofambit.com>
2024-02-27 15:54:13 -0800

    add libraries example data and index page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
 import { tutorialCollection } from "./tutorials/config";
 import { pastEventsCollection } from "./past-events/config";
+import { librariesCollection } from "./libraries/config";
 
 export const collections = {
   tutorials: tutorialCollection,
   "past-events": pastEventsCollection,
+  libraries: librariesCollection,
 };
