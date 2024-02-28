ad16c1f304577b1168fa778e2dfb81f8b81dfbec
evelyn masso <coding@outofambit.com>
2024-02-27 17:32:26 -0800

    tutorialCollection -> tutorialsCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
-import { tutorialCollection } from "./tutorials/config";
+import { tutorialsCollection } from "./tutorials/config";
 import { pastEventsCollection } from "./past-events/config";
 import { librariesCollection } from "./libraries/config";
 import { peopleCollection } from "./people/config";
 import { sketchesCollection } from "./sketches/config";
 
 export const collections = {
-  tutorials: tutorialCollection,
+  tutorials: tutorialsCollection,
   "past-events": pastEventsCollection,
   libraries: librariesCollection,
   people: peopleCollection,
