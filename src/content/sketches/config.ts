e10fe3695f06a98ea3bc52c178aa5440b93ea0ab
evelyn masso <coding@outofambit.com>
2024-02-28 13:50:24 -0800

    move markdown body to yaml values

--- src/content/sketches/config.ts
+++ src/content/sketches/config.ts
@@ -2,12 +2,21 @@ import { z, defineCollection, reference } from "astro:content";
 import { author, image } from "../shared";
 
 export const sketchesCollection = defineCollection({
-  type: "content",
+  type: "data",
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
     thumbnailImage: image(),
     relatedSketches: z.array(reference("sketches")).optional(),
     author: author(),
+    embed: z.object({
+      url: z.string().url(),
+      width: z.number().int(),
+      height: z.number().int(),
+    }),
+    license: z.object({
+      name: z.string(),
+      url: z.string().url().optional(),
+    }),
   }),
 });
