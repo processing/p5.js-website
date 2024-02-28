423e4862143a543bee25a59c201aefcc5365adc5
evelyn masso <coding@outofambit.com>
2024-02-27 17:07:01 -0800

    add sketches collection, schema, route

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,10 +2,12 @@ import { tutorialCollection } from "./tutorials/config";
 import { pastEventsCollection } from "./past-events/config";
 import { librariesCollection } from "./libraries/config";
 import { peopleCollection } from "./people/config";
+import { sketchesCollection } from "./sketches/config";
 
 export const collections = {
   tutorials: tutorialCollection,
   "past-events": pastEventsCollection,
   libraries: librariesCollection,
   people: peopleCollection,
+  sketches: sketchesCollection,
 };
---
+++ src/content/sketches/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection, reference } from "astro:content";
+
+export const sketchesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    featuredImage: z
+      .object({
+        url: z.string().url(),
+        altText: z.string(),
+      })
+      .optional(),
+    relatedSketches: z.array(reference("sketches")).optional(),
+    author: z.object({
+      name: z.string(),
+      url: z.string().url().optional(),
+    }),
+  }),
+});
