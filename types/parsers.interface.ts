932b00077a8a05999846ce2e3e3dc1fc4c57864c
Stalgia Grigg <stalgia@bocoup.com>
2024-02-28 19:34:18 -0800

    Inline documentation for parsing types

--- types/parsers.interface.ts
+++ types/parsers.interface.ts
@@ -1,3 +1,4 @@
+/* Defines the structure for the larger p5 project information */
 export interface Project {
   name: string;
   description: string;
@@ -5,6 +6,7 @@ export interface Project {
   version: string;
 }
 
+/* Detailed information about a file within within project */
 export interface FileDetail {
   name: string;
   modules: Record<string, boolean>;
@@ -13,92 +15,92 @@ export interface FileDetail {
   namespaces: Record<string, unknown>;
 }
 
+/* Describes a module within the project */
 export interface Module {
-  name: string;
-  submodules: Record<string, Module>;
-  classes: Record<string, boolean>;
-  fors: Record<string, boolean>;
-  namespaces: Record<string, unknown>;
-  module: string;
-  file: string;
-  line: number;
-  description: string;
-  tag?: string;
-  itemtype?: string;
-  requires?: string[];
+  name: string; // The name of the module.
+  submodules: Record<string, Module>; // Nested modules within this module.
+  classes: Record<string, boolean>; // A record of classes in the module.
+  namespaces: Record<string, unknown>; // Namespaces declared within the module.
+  module: string; // The parent module name.
+  file: string; // The file where the module is defined.
+  line: number; // The line number where the module definition starts.
+  description: string; // A description of the module.
+  tag?: string; // An optional tag for additional categorization.
+  requires?: string[]; // Optional array of module dependencies.
 }
 
+/* A mapping of module names to their corresponding Module definition */
 export interface Modules {
   [moduleName: string]: Module;
 }
 
+/* Describes a parameter for methods or constructors */
 interface Param {
-  name: string;
-  description: string;
-  type: string;
-  optional?: boolean;
+  name: string; // The parameter name.
+  description: string; // A description of the parameter.
+  type: string; // The data type of the parameter.
+  optional?: boolean; // Indicates if the parameter is optional.
 }
 
-export interface ReferenceClassDefinition {
-  name: string;
-  shortname: string;
-  classitems: Param[];
-  plugins: string[];
-  extensions: string[];
-  plugin_for: string[];
-  extension_for: string[];
-  module: string;
-  submodule: string;
-  namespace: string;
-  file?: string;
-  line?: number;
-  description: string;
-  is_constructor?: boolean;
-  params?: Param[];
-  example?: string[];
-  chainable?: number;
+/* Represents the definition of a class within the project, including its properties, methods, and inheritance information. */
+export interface ReferenceClassDefinition extends Chainable {
+  name: string; // The name of the class.
+  shortname: string; // A shorter or abbreviated name for the class.
+  classitems: Param[]; // Parameters or properties of the class.
+  plugins: string[]; // Plugins associated with the class.
+  extensions: string[]; // Extensions of the class.
+  plugin_for: string[]; // Classes that this class is a plugin for.
+  extension_for: string[]; // Classes that this class extends.
+  module: string; // The module that includes this class.
+  submodule: string; // The submodule containing this class.
+  namespace: string; // The namespace of the class.
+  file?: string; // Optional file where the class is defined.
+  line?: number; // Optional line number where the class definition starts.
+  description: string; // A description of the class.
+  is_constructor?: boolean; // Indicates if this class is a constructor.
+  params?: Param[]; // Constructor parameters, if applicable.
+  example?: string[]; // Example usage of the class.
 }
 
+/* A collection of ReferenceClassDefinition, indexed by class name */
 export interface ReferenceClass {
   [className: string]: ReferenceClassDefinition;
 }
 
+/* Base properties shared by both method and property items within a class */
 interface BaseClassItem {
-  file: string;
-  line: number;
-  class: string;
-  description?: string;
-  itemtype: "method" | "property";
-  name: string;
-  module: string;
-  submodule: string;
-  alt?: string;
-  example?: string[];
+  file: string; // The file where the item is defined.
+  line: number; // The line number where the item definition starts.
+  class: string; // The class name to which the item belongs.
+  description?: string; // Optional description of the item.
+  itemtype: "method" | "property"; // Specifies whether the item is a method or a property.
+  name: string; // The name of the item.
+  module: string; // The module containing this item.
+  submodule: string; // The submodule containing this item.
+  alt?: string; // Alt text for the example
+  example?: string[]; // Examples of the item's usage.
 }
 
+/* Describes whether a method or constructor is chainable */
 interface Chainable {
   chainable: number;
 }
 
-interface Param {
-  name: string;
-  description: string;
-  type: string;
-  optional?: boolean;
-}
-
+/* Represents the return value of a method or constructor */
 interface Return {
   description: string;
   type: string;
 }
 
+/* Represents a method within a class */
 export interface ReferenceClassItemMethod extends BaseClassItem, Chainable {
   params?: Param[];
   return?: Return;
   example?: string[];
-  overloads?: MethodOverload[];
+  overloads?: MethodOverload[]; // Optional array of method overloads.
 }
 
+/* Describes the parameters and return value of a method overload */
 interface MethodOverload {
   line: number;
   params?: Param[];
@@ -106,18 +108,22 @@ interface MethodOverload {
   chainable?: number;
 }
 
+/* Represents a property within a class */
 export interface ReferenceClassItemProperty extends BaseClassItem {
   type: string;
 }
 
+/* A union type representing either a method or a property within a class */
 export type ReferenceClassItem =
   | ReferenceClassItemMethod
   | ReferenceClassItemProperty;
 
+/* Global constants defined within the project */
 export interface ParsedConsts {
   [key: string]: string[];
 }
 
+/* The parsed output of the YUIDoc JSON file */
 export interface ParsedLibraryReference {
   project: Project;
   files: Record<string, FileDetail>;
