8284b21b79f0ba17a3a049b2f9d1138fb47aa2c5
Stalgia Grigg <stalgia.grigg@gmail.com>
2024-02-29 16:53:32 -0800

    Add Examples Content Collection (#8)
    * Parser in new approach
    
    * Working build
    
    * Able to access reference content
    
    * Add description, correct routing in index
    
    * Simplify builder
    
    * Wrangling types
    
    * Inline docs, type renames, rearranging properties
    
    * Add todo comment
    
    * Inline documentation for parsing types
    
    * Make linter and checker happy, change operation slugs, handle types
    
    * Initial content push
    
    * Restructure examples content
    
    * Access all examples through Astro
    
    * Correctly rendering examples
    
    * Clean up, don't lint examples
    
    * Add dist to eslintignore

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -122,7 +122,33 @@ export const readFile = async (filePath: string) => {
 };
 
 /**
- * Wrapper around `fs.cp` that catches errors
+ * Get all the file paths in a directory and its subdirectories
+ * @param directory Top-level directory to search for files
+ * @returns string[] an array of all the file paths in the directory and its subdirectories
+ */
+export const getAllFiles = async (directory: string) => {
+  const files: string[] = [];
+
+  // Function to recursively read the directory and its subdirectories
+  const readDirectory = async (dir: string) => {
+    const entries = await fs.readdir(dir, { withFileTypes: true });
+
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+
+      if (entry.isDirectory()) {
+        await readDirectory(fullPath);
+      } else {
+        files.push(fullPath);
+      }
+    }
+  };
+
+  await readDirectory(directory);
+  return files;
+};
+
+/* Wrapper around `fs.cp` that catches errors
  * @param filePath Path to the file
  * @param destinationPath Where to copy it
  * @param cpOptions Options passed to `fs.cp`
