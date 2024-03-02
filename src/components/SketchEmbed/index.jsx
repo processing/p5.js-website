90c59d99128b9b6d1b12599328e05754c2cf138e
Dave Pagurek <davepagurek@gmail.com>
2024-03-02 08:51:39 -0500

    Convert the first WebGL tutorial

---
+++ src/components/SketchEmbed/index.jsx
@@ -0,0 +1,21 @@
+import { CodeFrame } from "../CodeEmbed/frame";
+
+/*
+ * A component that displays a full-width sketch without showing its code.
+ * props: {
+ *   code: string;
+ *   cssCode?: string;
+ *   bodyCode?: string;
+ *   width?: number | string;
+ *   height?: number | string;
+ * }
+ */
+export const SketchEmbed = (props) => (
+  <CodeFrame
+    code={props.code}
+    cssCode={props.cssCode}
+    bodyCode={props.bodyCode}
+    height={props.height || 400}
+    width="100%"
+  />
+);
