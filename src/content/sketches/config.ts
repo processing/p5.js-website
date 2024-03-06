c02e6f86e1e2b51f603b41e33807683dc17c9ff6
cypress evelyn masso <coding@outofambit.com>
2024-03-06 11:24:34 -0800

    Some Inline docs for content collection + schema changes based on latest UI designs (#15)
    * add docs for content collections
    
    * adjust schemas for UI design iteration

--- src/content/sketches/config.ts
+++ src/content/sketches/config.ts
@@ -1,21 +1,34 @@
 import { z, defineCollection, reference } from "astro:content";
 import { author, image } from "../shared";
 
+/**
+ * Content collection for the Sketches showcase section of the site.
+ */
 export const sketchesCollection = defineCollection({
   type: "data",
   schema: z.object({
+    // Title of the sketch
     title: z.string(),
+    // Short description of the sketch
     description: z.string().optional(),
+    // Thumbail image to be shown in sketches overview page
     thumbnailImage: image(),
+    // Other sketches related to this one (use their slug)
     relatedSketches: z.array(reference("sketches")).optional(),
     author: author(),
+    // Information to embed the sketch from an external source
     embed: z.object({
+      // Embed url to load in an iframe
       url: z.string().url(),
+      // How wide to make the iframe
       width: z.number().int(),
+      // How tall to make the iframe
       height: z.number().int(),
     }),
     license: z.object({
+      // Name of the license
       name: z.string(),
+      // Url to the text of the license itself
       url: z.string().url().optional(),
     }),
   }),
