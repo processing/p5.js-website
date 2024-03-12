d750692a83bdc67b8ac2ad890c028f221a995bfa
evelyn masso <coding@outofambit.com>
2024-03-12 14:27:47 -0700

    Update _utils.test.ts

--- test/pages/_utils.test.ts
+++ test/pages/_utils.test.ts
@@ -2,7 +2,14 @@ import { expect, test, suite } from "vitest";
 import { exampleContentSlugToLegacyWebsiteSlug } from "../../src/pages/_utils";
 
 suite("exampleContentSlugToLegacyWebsiteSlug", () => {
-  test("works for english slugs", () => {
+  test("works for prefixed english slugs", () => {
+    expect(
+      exampleContentSlugToLegacyWebsiteSlug(
+        "en/00_structure/01_coordinates/description",
+      ),
+    ).toBe("en/structure-coordinates.html");
+  });
+  test("works for un-prefixed english slugs", () => {
     expect(
       exampleContentSlugToLegacyWebsiteSlug(
         "00_structure/01_coordinates/description",
