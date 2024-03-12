a0126e4eca1fda39126b26170a47d94296e3ec92
evelyn masso <coding@outofambit.com>
2024-03-11 17:16:31 -0700

    move routing logics to util helper functions

--- src/pages/utils.ts
+++ src/pages/utils.ts
@@ -4,6 +4,7 @@ import {
   type AnyEntryMap,
 } from "astro:content";
 import { defaultLocale, supportedLocales } from "../../const";
+import { readFile } from "fs/promises";
 
 export const getCollectionInDefaultLocale = async <C extends keyof AnyEntryMap>(
   collectionName: C,
@@ -45,4 +46,36 @@ export const exampleContentSlugToLegacyWebsiteSlug = (path: string): string =>
     .replace(/^en\/\d+_(.*?)\/\d+_(.*?)\/description$/, "/$1-$2.html")
     .replace(/_/g, "-");
 
-export default exampleContentSlugToLegacyWebsiteSlug;
+/**
+ * Returns the code sample needed for the example given.
+ *
+ * @param exampleId
+ * @returns
+ */
+export const getExampleCode = async (exampleId: string): Promise<string> => {
+  const codePath = `src/content/examples/${exampleId.replace("description.mdx", "code.js")}`;
+  const code = await readFile(codePath, "utf-8");
+  return code;
+};
+
+const contributorDocIndexPageName = "readme";
+/**
+ * If the given slug is the slug of the entry in the contributor doc
+ * collection that we want to use as the index page, this returns a
+ * '/' slug for routing purposes. Otherwise, just returns the slug given,
+ * unchanged.
+ *
+ * For example: `contributor-docs/es/` will show
+ * the content from `contributor-docs/es/readme`
+ */
+export const convertContributorDocIndexSlugIfNeeded = (slug: string) =>
+  slug.endsWith(contributorDocIndexPageName)
+    ? slug.slice(0, -contributorDocIndexPageName.length)
+    : slug;
+
+/**
+ * We cannot use Astro's default slug because it removes characters like '.'
+ * We use the id instead and remove the file extension
+ */
+export const makeReferencePageSlug = (id: string): string =>
+  id.replace(/\.mdx$/, "");
