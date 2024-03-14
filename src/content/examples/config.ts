e3704fcec32657ecfaf40a0a38808e608668dd5f
Stalgia Grigg <stalgia@bocoup.com>
2024-03-14 16:41:30 -0700

    Correct typing

--- src/content/examples/config.ts
+++ src/content/examples/config.ts
@@ -2,16 +2,21 @@ import { z, defineCollection } from "astro:content";
 import { relatedContent } from "../shared";
 
 /**
+ * Schema for the Example content type.
+ */
+export const exampleSchema = z.object({
+  // Title of the example
+  title: z.string(),
+  // Aria label used for the live example code
+  arialabel: z.string().optional(),
+  relatedContent: relatedContent().optional(),
+});
+
+/**
  * Content collection for the Examples section of the site.
  * Each file represents a single example.
  */
 export const examplesCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    // Title of the example
-    title: z.string(),
-    // Aria label used for the live example code
-    arialabel: z.string().optional(),
-    relatedContent: relatedContent().optional(),
-  }),
+  schema: exampleSchema,
 });
