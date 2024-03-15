af3516c5af7d1f151bc6a6ac55086d09fb05937f
cypress evelyn masso <coding@outofambit.com>
2024-03-14 17:35:24 -0700

    supported locale matching regex :) (#30)
    * supported locale matching regex
    
    * Update _utils.test.ts

--- test/pages/_utils.test.ts
+++ test/pages/_utils.test.ts
@@ -50,3 +50,41 @@ suite("removeLocaleAndExtensionFromId", () => {
     );
   });
 });
+
+suite("localeMatchingRegex", () => {
+  test("matches locale paths", () => {
+    const re = localeMatchingRegex();
+    expect("/es/examples/number".match(re)).toHaveLength(1);
+    expect("/es/examples/number/".match(re)).toHaveLength(1);
+    expect("es/examples/number/".match(re)).toHaveLength(1);
+    expect("es/examples/number".match(re)).toHaveLength(1);
+  });
+  test("matches default locale paths", () => {
+    const re = localeMatchingRegex();
+    expect("/en/examples/number".match(re)).toHaveLength(1);
+    expect("/en/examples/number/".match(re)).toHaveLength(1);
+    expect("en/examples/number/".match(re)).toHaveLength(1);
+    expect("en/examples/number".match(re)).toHaveLength(1);
+  });
+  test("matches locale alone", () => {
+    const re = localeMatchingRegex();
+    expect("/es".match(re)).toHaveLength(1);
+    expect("es/".match(re)).toHaveLength(1);
+    expect("/es/".match(re)).toHaveLength(1);
+    expect("/en".match(re)).toHaveLength(1);
+    expect("en/".match(re)).toHaveLength(1);
+    expect("/en/".match(re)).toHaveLength(1);
+  });
+  test("doesnt match unsupported locale prefixes", () => {
+    const re = localeMatchingRegex();
+    expect("/oo/".match(re)).toBeNull();
+    expect("/oo-PL/".match(re)).toBeNull();
+  });
+  test("doesnt match near urls", () => {
+    const re = localeMatchingRegex();
+    expect("/english/index".match(re)).toBeNull();
+    expect("/features/es".match(re)).toBeNull();
+    expect("/features/en/".match(re)).toBeNull();
+    expect("/features/es/family/".match(re)).toBeNull();
+  });
+});
