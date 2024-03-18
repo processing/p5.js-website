e5da6363865cecdb2d239346e337c8f1e332ff5e
cypress evelyn masso <coding@outofambit.com>
2024-03-18 15:15:30 -0700

    Locale Switcher Component (#25)
    * more locale utils
    
    * locale switcher component
    
    * remove node deps from browser file utils
    
    * add locale switcher to base layout
    
    * add spanish homepage for testing
    
    * clean up locale path utils
    
    * fix locale url handling utils, put under test
    
    * base page slugs only go one level deep
    
    * update localized past events template
    
    * docs
    
    * refactor away unneeded util
    
    * add more tests
    
    * dont generate extra root pages
    
    * import aliases
    
    * upgrade astro

--- test/pages/_utils.test.ts
+++ test/pages/_utils.test.ts
@@ -4,7 +4,9 @@ import {
   exampleContentSlugToLegacyWebsiteSlug,
   makeReferencePageSlug,
   localeMatchingRegex,
-} from "../../src/pages/_utils";
+  reformUrlforNewLocale,
+  splitLocaleFromPath,
+} from "@pages/_utils";
 
 suite("exampleContentSlugToLegacyWebsiteSlug", () => {
   test("works for prefixed english slugs", () => {
@@ -86,5 +88,66 @@ suite("localeMatchingRegex", () => {
     expect("/features/es".match(re)).toBeNull();
     expect("/features/en/".match(re)).toBeNull();
     expect("/features/es/family/".match(re)).toBeNull();
+  });
+});
+
+suite("reformUrlforNewLocale", () => {
+  test("preserves slashes", () => {
+    expect(reformUrlforNewLocale("/examples/number", "es")).toBe(
+      "/es/examples/number",
+    );
+    expect(reformUrlforNewLocale("/examples/number/", "es")).toBe(
+      "/es/examples/number/",
+    );
+  });
+  test("works for switching collection url locale", () => {
+    expect(reformUrlforNewLocale("/past-events/example", "es")).toBe(
+      "/es/past-events/example",
+    );
+    expect(reformUrlforNewLocale("/es/past-events/example", "en")).toBe(
+      "/past-events/example",
+    );
+  });
+  test("works for switching collection index locale", () => {
+    expect(reformUrlforNewLocale("/reference/", "es")).toBe("/es/reference/");
+    expect(reformUrlforNewLocale("/es/reference/", "en")).toBe("/reference/");
+  });
+  test("works for switching locale at /", () => {
+    expect(reformUrlforNewLocale("/", "es")).toBe("/es/");
+    expect(reformUrlforNewLocale("/es/", "en")).toBe("/");
+  });
+});
+
+suite("splitLocaleFromPath", () => {
+  test("extracts path", () => {
+    expect(splitLocaleFromPath("/es/examples/number")[1]).toBe(
+      "/examples/number",
+    );
+    expect(splitLocaleFromPath("/es/examples/number/")[1]).toBe(
+      "/examples/number/",
+    );
+  });
+  test("handles /", () => {
+    expect(splitLocaleFromPath("/")[1]).toBe("/");
+    expect(splitLocaleFromPath("/")[0]).toBe("en");
+    expect(splitLocaleFromPath("/es/")[0]).toBe("es");
+    expect(splitLocaleFromPath("/es/")[1]).toBe("/");
+    expect(splitLocaleFromPath("/es")[0]).toBe("es");
+    expect(splitLocaleFromPath("/es")[1]).toBe("/");
+  });
+  test("detects prefixed locale", () => {
+    expect(splitLocaleFromPath("/es/examples/number")[0]).toBe("es");
+    expect(splitLocaleFromPath("/en/past-events/example")[0]).toBe("en");
+  });
+  test("detects un-prefixed locale", () => {
+    expect(splitLocaleFromPath("examples/number")[0]).toBe("en");
+  });
+  test('works for "relative" paths', () => {
+    expect(splitLocaleFromPath("examples")[0]).toBe("en");
+    expect(splitLocaleFromPath("examples")[1]).toBe("examples");
+    expect(splitLocaleFromPath("en")[0]).toBe("en");
+    expect(splitLocaleFromPath("en")[1]).toBe("/");
+    expect(splitLocaleFromPath("es")[0]).toBe("es");
+    expect(splitLocaleFromPath("es")[1]).toBe("/");
   });
 });
