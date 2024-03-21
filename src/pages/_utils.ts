4e3ea6c1b14ce294c3d43a912c4561a31e2005d8
cypress evelyn masso <coding@outofambit.com>
2024-03-21 10:38:54 -0700

    i18n utils folder (#38)
    * reorganize i18n helpers

--- src/pages/_utils.ts
+++ src/pages/_utils.ts
@@ -4,7 +4,8 @@ import {
   type AnyEntryMap,
   type ContentEntryMap,
 } from "astro:content";
-import { defaultLocale, supportedLocales } from "../../const";
+import { defaultLocale } from "@i18n/const";
+import { removeLocalePrefix, startsWithSupportedLocale } from "@i18n/utils";
 
 /**
  * Retreives all the entries in the given collection, filtered to only include
@@ -80,73 +81,6 @@ export const getCollectionInLocale = async <C extends keyof AnyEntryMap>(
   await getCollection(collectionName, ({ id }) => id.startsWith(`${locale}/`));
 
 /**
- * Checks if a collecion entry's slug begins with a locale prefix
- * (for example: 'es/')
- *
- * @param slug
- * @returns
- */
-export const startsWithSupportedLocale = (slug: string) => {
-  for (const loc of supportedLocales) {
-    if (slug.startsWith(`${loc}/`)) return true;
-  }
-  return false;
-};
-
-/**
- * Splits the locale prefix out of a slug, and
- * returns the two as separate strings.
- * **Note: only handles absolute paths!**
- *
- * @param slug
- * @returns a tuple of the locale and the new slug
- */
-export const splitLocaleFromPath = (path: string): [string, string] => {
-  for (const loc of supportedLocales) {
-    const localeRegex = new RegExp(`^/?${loc}(?:/|$)`, "i");
-    const matched = path.match(localeRegex);
-    if (matched !== null) return [loc, path.replace(localeRegex, "/")];
-  }
-  return [defaultLocale, path];
-};
-
-/**
- * Removes the default locale prefix from a slug (if its there)
- *
- * @param slug
- * @returns
- */
-export const removeLocalePrefix = (prefixedPath: string): string =>
-  splitLocaleFromPath(prefixedPath)[1];
-
-/**
- * Turns a given url (of any locale) into a url within the given locale
- * **Note: only handles absolute paths!**
- *
- * @param url
- * @param newLocale
- * @returns
- */
-export const reformUrlforNewLocale = (url: string, newLocale: string) => {
-  const unPrefixedUrl = removeLocalePrefix(url);
-  if (newLocale === defaultLocale) {
-    return `${unPrefixedUrl}`;
-  }
-  return `/${newLocale}${unPrefixedUrl}`;
-};
-
-/**
- * Gets the current locale by parsing it out of the current url.
- * **Note: Can only be used client-side!**
- *
- * @returns
- */
-export const getCurrentLocale = (path: undefined | string): string => {
-  const [locale] = splitLocaleFromPath(path || window.location.pathname);
-  return locale;
-};
-
-/**
  * Astro automatically uses the directory structure for slug information
  * Historically the p5 website has used a different structure for example file vs. webpage routing
  * This function transforms the Astro slug to the appropriate webpage route to avoid breaking
@@ -198,14 +132,6 @@ export const transformExampleSlugs = <C extends keyof ContentEntryMap>(
 
   return transformedEntries;
 };
-
-/**
- * Creates a regex that matches any of the supported locale codes
- * at the beginning of a path or slug. Also matches an optional `/`
- * before and an optional `/` after.
- */
-export const localeMatchingRegex = () =>
-  new RegExp(`^/?(?:${supportedLocales.join("|")})(?:/|$)`);
 
 /**
  * Returns the correct URL to link to for a libary entry
