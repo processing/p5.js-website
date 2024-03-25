821ad18b21a5da130e10d7f451047b49845d23d1
evelyn masso <coding@outofambit.com>
2024-03-24 22:00:36 -0700

    add test for code embed component

---
+++ test/mocks/server.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "fs";
+import { HttpResponse, http } from "msw";
+import { setupServer } from "msw/node";
+
+// Define handlers that catch the corresponding requests and returns the mock data.
+const handlers = [
+  http.get("https://cdnjs.cloudflare.com/ajax/libs/p5.js/*", () => {
+    return HttpResponse.text(
+      readFileSync("./p5.min.js", { encoding: "utf-8" }),
+    );
+  }),
+];
+
+// This configures a Service Worker with the given request handlers.
+export const server = setupServer(...handlers);
