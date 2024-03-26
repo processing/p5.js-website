5458fa697422c56500324dde7937a0f2fc39fa24
Stalgia Grigg <stalgia@bocoup.com>
2024-03-26 16:40:44 -0700

    Update pathing

--- src/scripts/builders/reference.ts
+++ src/scripts/builders/reference.ts
@@ -128,9 +128,23 @@ const addDocToModulePathTree = (
     $("a").each(function () {
       let href = $(this).attr("href");
       if (!href) return;
-      if (href.startsWith("#/")) {
+      // If the href starts with the class prefix
+      if (href.startsWith("#/p5.")) {
+        const parts = href.split("/");
+        // Check to see if the last part of the href is a method class
+        if (parts[parts.length - 1].indexOf("p5.") === -1) {
+          // If it is a method class, replace the prefix with /reference/
+          href = href.replace("#/", "/reference/");
+        } else {
+          // If it is a class itself, replace the prefix with /reference/p5/
+          href = href.replace("#/", "/reference/p5/");
+        }
+      } else if (href.startsWith("#/")) {
+        // If the href starts with #/, replace it with /reference/
         href = href.replace("#/", "/reference/");
       } else if (href.startsWith("/reference/#")) {
+        // p5 sound sometimes uses /reference/#/ which is incorrect
+        // Replace it with /reference/
         href = href.replace("/reference/#", "/reference/");
       }
       $(this).attr("href", href);
