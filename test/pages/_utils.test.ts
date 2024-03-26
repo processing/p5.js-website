dba65f88a4d91f6b4eba96cb28c9151949aab6d2
Stalgia Grigg <stalgia.grigg@gmail.com>
2024-03-26 13:58:35 -0700

    Inline links reference and URL concatenation fixes (#64)
    * Updated reference
    
    * First attempt at correcting JSON comment links
    
    * Generate paths using removeLocaleAndExtensionFromId
    
    * Rename removeLocaleAndExtensionFromId symbol

--- test/pages/_utils.test.ts
+++ test/pages/_utils.test.ts
@@ -2,7 +2,9 @@ import { expect, test, suite } from "vitest";
 import {
   convertContributorDocIndexSlugIfNeeded,
   exampleContentSlugToLegacyWebsiteSlug,
-  makeReferencePageSlug,
+  removeContentFileExt,
+  removeLeadingSlash,
+  removeLocaleAndExtension,
 } from "@pages/_utils";
 
 suite("exampleContentSlugToLegacyWebsiteSlug", () => {
@@ -39,13 +41,30 @@ suite("convertContributorDocIndexSlugIfNeeded", () => {
   });
 });
 
-suite("makeReferencePageSlug", () => {
+suite("removeContentFileExt", () => {
   test("handles prefixed english slugs", () => {
-    expect(makeReferencePageSlug("en/p5.AudioIn/amp.mdx")).toBe(
+    expect(removeContentFileExt("en/p5.AudioIn/amp.mdx")).toBe(
       "en/p5.AudioIn/amp",
     );
   });
   test("handles un-prefixed english slugs", () => {
-    expect(makeReferencePageSlug("p5.AudioIn/amp.mdx")).toBe("p5.AudioIn/amp");
+    expect(removeContentFileExt("p5.AudioIn/amp.mdx")).toBe("p5.AudioIn/amp");
+  });
+});
+
+suite("removeLeadingSlash", () => {
+  test("removes leading slash", () => {
+    expect(removeLeadingSlash("/path/to/file")).toBe("path/to/file");
+  });
+  test("does nothing if no leading slash", () => {
+    expect(removeLeadingSlash("path/to/file")).toBe("path/to/file");
+  });
+});
+
+suite("removeLocaleAndExtensionFromId", () => {
+  test("removes locale and extension", () => {
+    expect(removeLocaleAndExtension("en/p5.AudioIn/amp.mdx")).toBe(
+      "p5.AudioIn/amp",
+    );
   });
 });
