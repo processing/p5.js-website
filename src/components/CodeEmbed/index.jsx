8c3d1dc73c00c2934da263f1df03ab200acd5aa0
Stalgia Grigg <stalgia@bocoup.com>
2024-04-01 17:58:59 -0700

    Small amount of code editor styles

--- src/components/CodeEmbed/index.jsx
+++ src/components/CodeEmbed/index.jsx
@@ -3,7 +3,6 @@ import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
 import { CodeFrame } from "./frame";
-
 /*
  * A more featured code embed component that uses CodeMirror
  *
@@ -26,37 +25,22 @@ export const CodeEmbed = (props) => {
   if (!rendered) return <div className="code-placeholder" />;
 
   return (
-    <>
-      <CodeMirror
-        value={codeString}
-        theme="light"
-        minimalSetup={{
-          highlightSpecialChars: false,
-          history: true,
-          drawSelection: true,
-          syntaxHighlighting: true,
-          defaultKeymap: true,
-          historyKeymap: true,
-        }}
-        indentWithTab={false}
-        extensions={[javascript()]}
-        onChange={(val) => setCodeString(val)}
-        editable={props.editable}
-        onCreateEditor={(editorView) =>
-          (editorView.contentDOM.ariaLabel = "Code Editor")
-        }
-      />
+    <div className="mb-md flex w-full flex-col overflow-hidden md:flex-row">
       {props.previewable ? (
-        <>
+        <div>
+          <CodeFrame jsCode={previewCodeString} width={150} height={200} />
+          {/* TODO: Actual button styles */}
           <button
+            className="bg-bg-gray-40 rounded-full p-xs"
             onClick={() => {
               console.log("updating code");
               setPreviewCodeString(codeString);
             }}
           >
-            Run Code
+            Run
           </button>
           <button
+            className="bg-bg-gray-40 rounded-full p-xs"
             onClick={() => {
               console.log("resetting code");
               setCodeString(initialCode);
@@ -65,10 +49,31 @@ export const CodeEmbed = (props) => {
           >
             Reset
           </button>
-          <CodeFrame jsCode={previewCodeString} />
-        </>
+        </div>
       ) : null}
-    </>
+      <div className="w-full md:w-[calc(100%-150px)]">
+        <CodeMirror
+          value={codeString}
+          theme="light"
+          width="100%"
+          minimalSetup={{
+            highlightSpecialChars: false,
+            history: true,
+            drawSelection: true,
+            syntaxHighlighting: true,
+            defaultKeymap: true,
+            historyKeymap: true,
+          }}
+          indentWithTab={false}
+          extensions={[javascript()]}
+          onChange={(val) => setCodeString(val)}
+          editable={props.editable}
+          onCreateEditor={(editorView) =>
+            (editorView.contentDOM.ariaLabel = "Code Editor")
+          }
+        />
+      </div>
+    </div>
   );
 };
 
