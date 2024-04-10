5cdcc3ca71c9eaf66849db20497a3ea0ee4dfd1c
Stalgia Grigg <stalgia@bocoup.com>
2024-04-09 17:57:14 -0700

    First pass mobile

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -13,6 +13,7 @@ export const Logo = ({ variant = "wordmark" }: LogoProps) => {
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
           viewBox="0 0 250 114"
+          height="100%"
           enableBackground="new 0 0 250 114"
         >
           <g>
