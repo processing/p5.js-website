5b7a4a27656e6322e1b4dea0ee9bf9476def6b62
Stalgia Grigg <stalgia@bocoup.com>
2024-04-10 12:27:54 -0700

    Address QA feedback

--- src/components/CodeEmbed/index.jsx
+++ src/components/CodeEmbed/index.jsx
@@ -33,12 +33,12 @@ export const CodeEmbed = (props) => {
 
   const updateOrReRun = () => {
     if (codeString === previewCodeString) {
-      setPreviewCodeString('');
+      setPreviewCodeString("");
       requestAnimationFrame(() => setPreviewCodeString(codeString));
     } else {
       setPreviewCodeString(codeString);
     }
-  }
+  };
 
   const [previewCodeString, setPreviewCodeString] = useState(codeString);
 
@@ -51,7 +51,7 @@ export const CodeEmbed = (props) => {
   return (
     <div className="my-md flex w-full flex-col overflow-hidden lg:flex-row">
       {props.previewable ? (
-        <div className="flex lg:flex-col">
+        <div className="ml-0 flex lg:flex-col">
           <CodeFrame
             jsCode={previewCodeString}
             width={props.previewWidth}
@@ -78,7 +78,7 @@ export const CodeEmbed = (props) => {
           </div>
         </div>
       ) : null}
-      <div className="relative ml-md w-full md:w-[calc(100%-150px)]">
+      <div className="relative w-full md:w-[calc(100%-var(--spacing-md))] lg:ml-md">
         <CodeMirror
           value={codeString}
           theme="light"
@@ -103,7 +103,7 @@ export const CodeEmbed = (props) => {
             (editorView.contentDOM.ariaLabel = "Code Editor")
           }
         />
-        <div className="absolute right-0 top-0 flex gap-xs p-xs">
+        <div className="absolute right-0 top-0 flex flex-col gap-xs p-xs md:flex-row">
           <CopyCodeButton textToCopy={codeString || initialCode} />
           <CircleButton
             onClick={() => {
