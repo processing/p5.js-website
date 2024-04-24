e89698ca37d660ba671836c8e320bbf5b0e5ecbd
limzykenneth <limzy.kenneth@gmail.com>
2024-04-24 20:54:45 +0100

    Fix localization dropdown to only include launch translations

--- src/i18n/const.ts
+++ src/i18n/const.ts
@@ -9,12 +9,9 @@ export const defaultLocale = "en";
  * All other locales supported by the website.
  */
 export const nonDefaultSupportedLocales = [
-  "ar",
   "es",
   "hi",
   "ko",
-  "sk",
-  "pt-br",
   "zh-Hans",
 ];
 
