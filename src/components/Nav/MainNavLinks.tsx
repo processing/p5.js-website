084d80c1461b0a1a8632cd558f44c44f47c4c67a
Stalgia Grigg <stalgia@bocoup.com>
2024-04-25 13:04:43 -0700

    Fix for lighthouse a11y issues, Home

--- src/components/Nav/MainNavLinks.tsx
+++ src/components/Nav/MainNavLinks.tsx
@@ -42,17 +42,23 @@ export const MainNavLinks = ({
       <a
         href="/"
         class={`${isHomepage ? "text-logo-color" : "text-sidebar-type-color"}`}
+        aria-label={isHomepage ? "Reload current page" : "Go to p5.js homepage"}
       >
         <Logo />
       </a>
-      <button class={styles.toggle} onClick={handleClick}>
+      <button
+        class={styles.toggle}
+        onClick={handleClick}
+        aria-hidden="true"
+        tabIndex={-1}
+      >
         <Icon kind={open ? "chevron-up" : "chevron-down"} />
       </button>
     </div>
   );
 
   return (
-    <div class={`${styles.mainlinks} ${open && "open"}`} aria-expanded={open}>
+    <div class={`${styles.mainlinks} ${open && "open"}`}>
       {renderLogo()}
       <ul>
         {links.map((link) => (
