c66eb4622b9f109bae7b2f90eb935e8861fed05d
evelyn masso <coding@outofambit.com>
2024-04-30 16:57:24 -0700

    show all categories in example page jump to links

--- src/pages/_utils.ts
+++ src/pages/_utils.ts
@@ -379,14 +379,13 @@ export const generateJumpToState = async (
             }) as JumpToLink,
         ),
       );
-
-      const hasCurrent = categoryLinks.some((link) => link.current);
-      // If the current entry is in this category, move this category to the top
-      if (hasCurrent) {
-        jumpToLinks.unshift(...categoryLinks);
-      } else {
-        jumpToLinks.push(...categoryLinks);
-      }
+    }
+    const hasCurrent = categoryLinks.some((link) => link.current);
+    // If the current entry is in this category, move this category to the top
+    if (hasCurrent) {
+      jumpToLinks.unshift(...categoryLinks);
+    } else {
+      jumpToLinks.push(...categoryLinks);
     }
   }
 
