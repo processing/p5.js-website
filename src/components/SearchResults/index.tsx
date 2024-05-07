e41d88bcab621c0b077a1817e7bb8c9345b42344
evelyn masso <coding@outofambit.com>
2024-05-06 17:59:28 -0700

    Update index.tsx

--- src/components/SearchResults/index.tsx
+++ src/components/SearchResults/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "preact/hooks";
+import { useMemo, useRef, useState } from "preact/hooks";
 import { Icon } from "../Icon";
 import { type JSX } from "preact";
 
