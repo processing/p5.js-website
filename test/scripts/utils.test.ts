0270cdf3175f0aae94b7fb38a35bc8b5d564cf75
Dave Pagurek <davepagurek@gmail.com>
2024-08-30 17:23:48 -0400

    Handle more cases, update tests

--- test/scripts/utils.test.ts
+++ test/scripts/utils.test.ts
@@ -16,19 +16,25 @@ test("rewriteRelativeMdLinks", () => {
   - [Other](./folder/document.md)
   - [Other](folder/document.md)
   - [lol](./some-where-else/wow/)
+  - [absolute](/wow/)
+  - [no trailing slash](../test)
+  - [external](https://p5js.org/)
   `),
   ).toEqual(`
   # Title
   
   You can find more examples [here](https://p5xjs.org/examples.html) and
-  in the [guide](./the-guide/).
+  in the [guide](../the-guide/).
   
   ![image](./assets/image.jpg)
   
   ## Related Docs
-  - See [Access](./access/).
-  - [Other](./folder/document/)
-  - [Other](folder/document/)
-  - [lol](./some-where-else/wow/)
+  - See [Access](../access/).
+  - [Other](../folder/document/)
+  - [Other](../folder/document/)
+  - [lol](../some-where-else/wow/)
+  - [absolute](/wow/)
+  - [no trailing slash](../test/)
+  - [external](https://p5js.org/)
   `);
 });
