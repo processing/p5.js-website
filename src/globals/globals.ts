4c5dc0064c600811ded760b223115712c66de782
Dave Pagurek <davepagurek@gmail.com>
2024-09-28 08:38:43 -0500

    Remove log

--- src/globals/globals.ts
+++ src/globals/globals.ts
@@ -15,7 +15,6 @@ export const eventsPerPage = 12 as const;
 
 export const cdnLibraryUrl =
   `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${p5Version}/p5.min.js` as const;
-  console.log(cdnLibraryUrl)
 export const fullDownloadUrl =
   `https://github.com/processing/p5.js/releases/download/v${p5Version}/p5.zip` as const;
 export const libraryDownloadUrl =
