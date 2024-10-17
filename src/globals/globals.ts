b37511d71529cc1a4054fc9c329c639afdbbfc3f
Dave Pagurek <davepagurek@gmail.com>
2024-10-17 17:11:28 -0400

    WIP: build docs from 2.0 dev branches

--- src/globals/globals.ts
+++ src/globals/globals.ts
@@ -14,7 +14,8 @@ export const sketchesPerPage = 12 as const;
 export const eventsPerPage = 12 as const;
 
 export const cdnLibraryUrl =
-  `https://cdn.jsdelivr.net/npm/p5@${p5Version}/lib/p5.min.js` as const;
+  `/p5.min.js` as const;
+  // `https://cdn.jsdelivr.net/npm/p5@${p5Version}/lib/p5.min.js` as const;
 export const fullDownloadUrl =
   `https://github.com/processing/p5.js/releases/download/v${p5Version}/p5.zip` as const;
 export const libraryDownloadUrl =
