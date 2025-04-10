792cfd00baf3e356a5cb1286a43a7915b1ac3272
Dave Pagurek <davepagurek@gmail.com>
2025-04-10 19:17:18 -0400

    Add comment

--- src/components/CodeEmbed/frame.tsx
+++ src/components/CodeEmbed/frame.tsx
@@ -42,6 +42,8 @@ canvas {
 }
 ${code.css || ""}
 </style>
+<!-- If we need an addon script, load p5 the usual way with no caching to make sure
+the import order doesn't get messed up. -->
 ${((code.scripts?.length ?? 0) > 0 ? [cdnLibraryUrl, ...(code.scripts ?? [])] : []).map((src) => `<script type="text/javascript" src="${src}"></script>`).join('\n')}
 <body>${code.htmlBody || ""}</body>
 <script id="code" type="text/javascript">${wrapSketch(code.js) || ""}</script>
