61f93bcf041bd4c9cb7deb50cf77ea59347fb1be
Dave Pagurek <davepagurek@gmail.com>
2025-04-17 12:52:43 -0400

    Update version

--- src/globals/p5-version.ts
+++ src/globals/p5-version.ts
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 export const p5Version = "1.11.8" as const;
+=======
+export const p5Version = "1.11.5" as const;
+>>>>>>> dd79321bd (Update version)
 export const p5SoundVersion = "0.1.0" as const;
