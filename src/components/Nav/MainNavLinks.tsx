44e6cfcccd9a9b045ccc1a926cc8b3e16f277904
Harsh <harshmastic@gmail.com>
2025-12-15 00:54:25 +0530

    fix: use hidden attribute for menu visibility and remove dynamic tabIndex

--- src/components/Nav/MainNavLinks.tsx
+++ src/components/Nav/MainNavLinks.tsx
@@ -76,18 +76,19 @@ export const MainNavLinks = ({
       <ul id="main-links-list" hidden={!isOpen}>
         {links.map((link) => (
           <li key={link.label}>
-            <a href={link.url} tabIndex={isOpen ? undefined : -1}>
-              {link.label}
-            </a>
+            <a href={link.url}>{link.label}</a>
           </li>
         ))}
       </ul>
-      <ul id="main-cta-list" class="flex flex-col gap-[15px]" hidden={!isOpen}>
+      <ul
+        id="main-cta-list"
+        class={`${isOpen ? "flex" : ""} flex-col gap-[15px]`}
+        hidden={!isOpen}
+      >
         <li>
           <a
             className={styles.buttonlink}
             href="https://editor.p5js.org"
-            tabIndex={isOpen ? undefined : -1}
           >
             <div class="mr-xxs">
               <Icon kind="code-brackets" />
@@ -99,7 +100,6 @@ export const MainNavLinks = ({
           <a
             className={styles.buttonlink}
             href="/donate/"
-            tabIndex={isOpen ? undefined : -1}
           >
             <div class="mr-xxs">
               <Icon kind="heart" />
