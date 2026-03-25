227685c497e0dffd839d406f90f7295f78c05a9c
nbogie <neillbogie@googlemail.com>
2026-03-25 04:09:09 +0000

    install playwright for automated browser-based testing
    npm init playwright@latest
    answers to questions:
    ts or js? ts
    dir: "tests/"
    CI? yes
    Install browsers? yes

---
+++ tests/example-recorded.spec.ts
@@ -0,0 +1,17 @@
+import { test } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('buy food');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('wash car');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('read book');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('listitem').filter({ hasText: 'wash car' }).getByLabel('Toggle Todo').check();
+  await page.getByRole('link', { name: 'Active' }).click();
+  await page.getByRole('link', { name: 'Completed' }).click();
+  await page.getByRole('link', { name: 'All' }).click();
+  await page.getByRole('button', { name: 'Clear completed' }).click();
+});
